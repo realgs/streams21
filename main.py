f55a988aff0df68bedc77c8fda3b0f49c56d58fb
ibockowsky <igor@newfantastic.com>
2021-03-01 19:43:57 +0100

    feat: implement logical body of script

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def is_array(array):
     if type(array) is not list:
         print("Arg isn't an array")
@@ -17,8 +20,6 @@ def is_array_of_numbers(array):
 
 
 def bubble_sort(array):
-    if is_array_of_numbers(array) is not True:
-        return print("The argument isn't an array or doesn't contain numbers")
     for i in range(len(array)):
         for j in range(0, len(array)-i-1):
             if array[j] > array[j+1]:
@@ -26,8 +27,6 @@ def bubble_sort(array):
 
 
 def insertion_sort(array):
-    if is_array_of_numbers(array) is not True:
-        return print("The argument isn't an array or doesn't contain numbers")
     for i in range(1, len(array)):
         temp = array[i]
         j = i-1
@@ -39,8 +38,25 @@ def insertion_sort(array):
         array[j+1] = temp
 
 
-test = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+def handler(array, alg='bubble'):
+    print('=====================')
+    if is_array_of_numbers(array) is not True:
+        return print("The argument isn't an array or doesn't contain numbers")
 
-print(test)
-insertion_sort(test)
-print(test)
+    print(f'Before sort: {array}')
+
+    if alg == 'bubble':
+        bubble_sort(array)
+    elif alg == 'insertion':
+        insertion_sort(array)
+
+    print(f'[{alg}] Sorted: {array}')
+    print('=====================')
+
+
+if __name__ == "__main__":
+    test_array_one = [randint(1, 50) for _ in range(10)]
+    test_array_two = [randint(1, 50) for _ in range(10)]
+
+    handler(test_array_one, 'bubble')
+    handler(test_array_two, 'insertion')
