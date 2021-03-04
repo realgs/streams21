1ec7346cf32eccffff85b4321d434a6ba57d833b
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-03-04 22:37:06 +0100

    added quick sort

--- sortowanie.py
+++ sortowanie.py
@@ -7,3 +7,24 @@ def insertion_sort(t):
             k -= 1
         t[k + 1] = el
     return t
+
+
+def quick_sort(t, x, y):
+    if x >= y:
+        return
+    i = x
+    j = y
+    v = t[x + (y-x) // 2]
+    while i <= j:
+        while v > t[i]:
+            i += 1
+        while v < t[j]:
+            j -= 1
+        if i <= j:
+            t[i], t[j] = t[j], t[i]
+            i += 1
+            j -= 1
+    if x < j:
+        quick_sort(t, x, j)
+    if i < y:
+        quick_sort(t, i, y)
