91d909853a61e393b56a7d2376b5a8475c51ca72
Bartosz Nocun <bartosznocun1@gmail.com>
2021-03-05 16:45:16 +0100

    Add display result

--- lista1/SelectionSort.py
+++ lista1/SelectionSort.py
@@ -3,3 +3,7 @@ def selectSort(arr):
         for j in range(i, len(arr)):
             if(arr[i] > arr[j]):
                 arr[i],arr[j] = arr[j],arr[i]
+
+numbers = [50, 32, 16, 24, 78, 13, 29, 89, 67, 22, 34, 86, 95, 19, 37]
+selectSort(numbers)
+print(f"Result: {numbers}")
