3e43de51551309aab74b36cf740bbca490d929c2
Bartosz Nocun <bartosznocun1@gmail.com>
2021-03-05 16:25:01 +0100

    Add quickSort method

--- lista1/QuickSort.py
+++ lista1/QuickSort.py
@@ -9,3 +9,14 @@ def partition(arr, start, end):
 
 	arr[i+1],arr[end] = arr[end],arr[i+1] 
 	return i+1
+
+def quickSort(arr, start, end): 
+	if start < end: 
+		p = partition(arr, start, end) 
+		quickSort(arr, start, p-1) 
+		quickSort(arr, p+1, end) 
+
+numbers = [50, 32, 16, 24, 78, 13, 29, 89, 67, 22, 34, 86, 95, 19, 37]
+length = len(numbers) 
+quickSort(numbers, 0, length-1) 
+print (f"Result: {numbers}")
