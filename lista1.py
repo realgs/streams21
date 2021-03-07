cdd53219f760799f7603acffd69749e86e3739c9
Blazej Raducki <raduckib@gmail.com>
2021-03-07 11:33:56 +0100

    Correcting

--- lista1.py
+++ lista1.py
@@ -4,8 +4,6 @@ def bubbleSorting(arr):
         replace = False
         for i in range(0, arrLen - 1):
             if arr[i] > arr[i + 1]:
-                # arr[i] = arr[i + 1]
-                # arr[i + 1] = arr[i]
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 replace = True
         arrLen -= 1
