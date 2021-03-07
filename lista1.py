f7c7486454511254ab1c28cca5025f298b026337
Blazej Raducki <raduckib@gmail.com>
2021-03-07 11:14:10 +0100

    Adding insert sorting

--- lista1.py
+++ lista1.py
@@ -9,8 +9,21 @@ def bubbleSorting(arr):
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 replace = True
         arrLen -= 1
-        print(arr)
         if replace == False: break
     return arr
 
-print(f'Final: {bubbleSorting([5, 6, -1, 0])}')
+
+print(f'Final: {bubbleSorting([3, 6, -2, 8, 4, 9, 11, 2])}')
+
+def insertSorting(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+print(f'Final: {insertSorting([3, 6, -2, 8, 4, 9, 11, 2])}')
