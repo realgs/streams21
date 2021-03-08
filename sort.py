1b06887709c51ebdf18a11ff684465647578b1d6
nataliahetmanska <natalia.hetmanska@gmail.com>
2021-03-08 10:45:45 +0100

    added test, additional random list of numbers + final touches

--- sort.py
+++ sort.py
@@ -1,33 +1,58 @@
 import random
 
-def bubble(list_of_num):
-    for i in range ( len ( list_of_num ) - 1, 0, -1 ):
+
+def bubble(num):
+    for i in range ( len ( num ) - 1, 0, -1 ):
         for j in range ( i ):
-            if list_of_num[j] > list_of_num[j + 1]:
-                temp = list_of_num[j]
-                list_of_num[j] = list_of_num[j + 1]
-                list_of_num[j + 1] = temp
+            if num[j] > num[j + 1]:
+                temp = num[j]
+                num[j] = num[j + 1]
+                num[j + 1] = temp
 
-    return list_of_num
+    return num
 
 
-def insertion(list_of_num):
-    for i in range ( 1, len ( list_of_num ) ):
+def insertion(num):
+    for i in range ( 1, len ( num ) ):
         j = i - 1
-        nxt = list_of_num[i]
+        nxt = num[i]
 
-        while (list_of_num[j] > nxt) and (j >= 0):
-            list_of_num[j + 1] = list_of_num[j]
+        while (num[j] > nxt) and (j >= 0):
+            num[j + 1] = num[j]
             j = j - 1
-        list_of_num[j + 1] = nxt
+        num[j + 1] = nxt
 
-    return list_of_num
+    return num
 
 
 list_of_num = [3, 15, 8, 26, 29, 54]
 
-sorted_list1 = bubble ( list_of_num )
-sorted_list2 = insertion ( list_of_num )
+random_list = []
+
+for m in range (10):
+    random_list.append (random.randint (1, 50))
+
+sorted_list1 = bubble (list_of_num)
+sorted_list2 = insertion (list_of_num)
+sorted_list3 = bubble (random_list)
+sorted_list4 = insertion (random_list)
+
+
+def test(sorted):
+    for k in range ( 0, len ( sorted ) - 1 ):
+
+        if sorted[k] > sorted[k + 1]:
+            print ("Sortowanie nie wykonało się prawidłowo")
+
+    print ("Sortowanie wykonało się prawidłowo")
+
+
+test (sorted_list1)
+test (sorted_list2)
+test (sorted_list3)
+test (sorted_list4)
 
 print(sorted_list1)
 print(sorted_list2)
+print(sorted_list3)
+print(sorted_list4)
