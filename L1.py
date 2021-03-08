0320ccf63801cb895d4810a2668ec771ef677f98
Bartosz-Stec <71563907+Bartosz-Stec@users.noreply.github.com>
2021-03-08 10:30:16 +0100

    Insert sorting

--- L1.py
+++ L1.py
@@ -2,3 +2,16 @@ from random import randint
 #prosze mnie nie bić, jutro skończe bo dziś miałem problemy z ukrytymi plikami
 #Bartosz Stec
 
+def insert(list):
+    for i in range(1, len(list)):
+        temp = list[i]
+        j = i - 1
+        while j >= 0 and list[j] > temp:
+            list[j + 1] = list[j]
+            j -= 1
+        list[j + 1] = temp
+    return list
+
+
+
+
