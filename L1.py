32804818f5bc807b00e9ddd7e2b62e5fbe0bd158
Bartosz-Stec <71563907+Bartosz-Stec@users.noreply.github.com>
2021-03-08 10:31:05 +0100

    Bubble sorting

--- L1.py
+++ L1.py
@@ -12,6 +12,20 @@ def insert(list):
         list[j + 1] = temp
     return list
 
+def bubble(list):
+    listLen = len(list)
+    while listLen > 1:
+        replace = False
+        for i in range(0, listLen - 1):
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
+                replace = True
+        listLen -= 1
+        if replace == False: break
+    return list
+
+
+
 
 
 
