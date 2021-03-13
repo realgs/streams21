30b9b8e27aaa80319ead4169dd0d25a782d7c7bd
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2021-03-13 14:41:38 +0100

    add drive code, format code

--- SortingMethods.py
+++ SortingMethods.py
@@ -10,6 +10,7 @@ def bubblesort(list, listLen):
             break
     return list
 
+
 def partition(arr, start, end):
     i = (start-1)
     pivot = arr[end]
@@ -22,6 +23,7 @@ def partition(arr, start, end):
     arr[i+1], arr[end] = arr[end], arr[i+1]
     return (i+1)
 
+
 def quickSort(arr, start, end):
     if len(arr) == 1:
         return arr
@@ -29,3 +31,10 @@ def quickSort(arr, start, end):
         p = partition(arr, start, end)
         quickSort(arr, start, p-1)
         quickSort(arr, p+1, end)
+
+
+list = [41, 91, 414, 156, 236, 24, 101, 9415, 24, 1, 81, 2, 9]
+list2 = list
+quickSort(list, 0, len(list)-1)
+print("Result of quicksort: ", list)
+print('Result of bubblesort: ', bubblesort(list2, len(list2)))
