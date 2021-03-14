ad2d000e363427e50731deeca805f853fb517004
Aleksandra_Jaroch <aleksandra.jaroch11@gmail.com>
2021-03-14 13:38:02 +0100

    added a sort check

--- sorting.py
+++ sorting.py
@@ -21,3 +21,13 @@ def insertion_sort(numbers):
 print(numbers)
 print(bubble_sort(numbers))
 print(insertion_sort(numbers))
+
+def sort_test(sorted_numbers):
+    for i in range(len(sorted_numbers)-1):
+        if sorted_numbers[i] <= sorted_numbers[i+1]:
+            continue
+        else:
+            print('Sort failed')
+
+sort_test(bubble_sort(numbers))
+sort_test(insertion_sort(numbers))
