bc5e3ae638815a635d656acf1926cc2146eff580
jankrzempek <jankrzempek9@gmail.com>
2021-03-14 16:53:55 +0100

    Fixes

--- sortowanie1.py
+++ sortowanie1.py
@@ -1,15 +1,20 @@
 #Bubble Sort
-array = [10,5,4,12,3,2,1]
-onOff = True
-while True:
-    for elements in range(0,len(array)-1):
-        if array[elements] > array[elements+1]:
-            var = array[elements]
-            array[elements] = array[elements+1]
-            array[elements+1] = var
-            onOff = False
-    if onOff == True:
-        print(array)
-        break;
-    if onOff == False:
+example = [10,5,4,12,3,2,1]
+
+def BubbleSort(array):
+    while True:
         onOff = True
+        
+        for elements in range(0,len(array)-1):
+            if array[elements] > array[elements+1]:
+                var = array[elements]
+                array[elements] = array[elements+1]
+                array[elements+1] = var
+                onOff = False
+        if onOff == True:
+            print(array)
+            break;
+        if onOff == False:
+            onOff = True
+
+BubbleSort(example)
--- sortowanie2.py
+++ sortowanie2.py
@@ -1,17 +1,22 @@
-array = [5,4,10,122,1,0,3,2,1]
+# Insert Sort
+example = [5,4,10,122,1,0,3,2,1]
 
-arrayOut = []
-tf = True
+def InsertSort(array):
 
-arrayOut.append(array[0])
-for elements in range(1,len(array)):
-    var = array[elements]
-    for inElem in range(len(arrayOut)):
-        if var < arrayOut[inElem]:
-            arrayOut.insert(inElem, var)
-            tf = False
-            break;
-    if tf == True:
-        arrayOut.append(var)
+    arrayOut = []
     tf = True
-print(arrayOut)
+
+    arrayOut.append(array[0])
+    for elements in range(1,len(array)):
+        var = array[elements]
+        for inElem in range(len(arrayOut)):
+            if var < arrayOut[inElem]:
+                arrayOut.insert(inElem, var)
+                tf = False
+                break;
+        if tf == True:
+            arrayOut.append(var)
+        tf = True
+    print(arrayOut)
+
+InsertSort(example)
