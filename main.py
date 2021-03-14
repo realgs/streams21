89b63c76d8011ed23779c04ae5584caa5252bd4b
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2021-03-14 23:46:14 +0100

    Added bubble sort

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def bubble_sort():
-  pass
+def bubble_sort(numbers):
+  l = numbers.copy()
+  for i in range(len(l)):
+    for j in range((len(l)-1)-i):
+      if l[j] > l[j + 1]:
+        temp = l[j]
+        l[j] = l[j + 1]
+        l[j + 1] = temp
+  return l
 
 def insertion_sort():
   pass
