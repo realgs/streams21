0841690f99353c943a57d9d1bead98ff7a368b3a
Piotr (pkk0) Uliński <pkk0@users.noreply.github.com>
2021-03-14 15:35:33 +0100

    Add sorting methods usage example

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def bubble_sort(array):
     array_size = len(array)
     for i in range(array_size):
@@ -21,3 +24,26 @@ def insertion_sort(array):
             j -= 1
 
         array[j + 1] = tmp
+
+
+def gen_random_array(size=10):
+    return [randint(-100, 100) for _ in range(size)]
+
+
+def run_usage_example():
+    first_array_to_sort, second_array_to_sort = gen_random_array(), gen_random_array()
+
+    print('Unsorted arrays:')
+    print(f'#1: {first_array_to_sort}')
+    print(f'#2: {second_array_to_sort}')
+
+    bubble_sort(first_array_to_sort)
+    insertion_sort(second_array_to_sort)
+
+    print('Sorted arrays:')
+    print(f'#1: {first_array_to_sort}')
+    print(f'#2: {second_array_to_sort}')
+
+
+if __name__ == '__main__':
+    run_usage_example()
