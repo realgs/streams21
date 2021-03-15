85ffb0901252f95493f6e7bdcab2c228e03d0ed4
Bartek Furtak <furtak.b@outlook.com>
2021-03-15 10:18:10 +0100

    edit program.py

--- program.py
+++ program.py
@@ -1,10 +1,11 @@
 import bubblesort
 import selectionsort
 
-list_of_nums = [5, 2, 1, 8, 4, 12, 15, 1, 0, 0]
+list_of_nums = [9, 21, 28, 21, 5, 2, 20, 18, 1, 8, 4, 12, 15, 1, 0, 0, 100, 125, 789, 18]
 bubblesort.bubble_sort(list_of_nums)
 print(list_of_nums)
 
-list_of_nums = [5, 2, 1, 8, 4, 12, 15, 1, 0, 0]
+
+list_of_nums = [9, 21, 28, 21, 5, 2, 20, 18, 1, 8, 4, 12, 15, 1, 0, 0, 100, 125, 789, 18]
 selectionsort.selection_sort(list_of_nums)
 print(list_of_nums)
