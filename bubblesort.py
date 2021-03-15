4bfbbab93d12ac96c63608cc43396e2ac5a7f9d5
Bartek Furtak <furtak.b@outlook.com>
2021-03-15 09:40:10 +0100

    add bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,9 @@
+def bubble_sort(nums):
+    
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
