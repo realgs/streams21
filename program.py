f8c51427ff308ad2fff8df40136b43cc21e85b24
Bartek Furtak <furtak.b@outlook.com>
2021-03-15 10:01:59 +0100

    complete selectionsort.py

--- program.py
+++ program.py
@@ -1,5 +1,10 @@
 import bubblesort
+import selectionsort
 
 list_of_nums = [5, 2, 1, 8, 4, 12, 15, 1, 0, 0]
 bubblesort.bubble_sort(list_of_nums)
+print(list_of_nums)
+
+list_of_nums = [5, 2, 1, 8, 4, 12, 15, 1, 0, 0]
+selectionsort.selection_sort(list_of_nums)
 print(list_of_nums)
