e5c74649947784e574888bf562029dfdbec54b30
Przemysław Zalewski <p.zalewski@int.pl>
2021-03-15 14:43:12 +0100

    add insert sort

--- lista1.py
+++ lista1.py
@@ -9,6 +9,7 @@ def main(n):
         lista_wartosci.append(random.randint(1, 10000))
     #print(lista_wartosci)
     bubble(lista_wartosci)
+    insert(lista_wartosci)
     return lista_wartosci
 
 
@@ -17,6 +18,18 @@ def bubble(lista_wartosci):
         for j in range(len(lista_wartosci)):
             if lista_wartosci[i] < lista_wartosci[j]:
                 lista_wartosci[i], lista_wartosci[j] = lista_wartosci[j], lista_wartosci[i]
+    print(lista_wartosci)
+    return lista_wartosci
+
+
+def insert(lista_wartosci):
+    for i in range(1, len(lista_wartosci)):
+        a = lista_wartosci[i]
+        j = i - 1
+        while j >= 0 and lista_wartosci[j] > a:
+            lista_wartosci[j + 1] = lista_wartosci[j]
+            j -= 1
+        lista_wartosci[j + 1] = a
     print(lista_wartosci)
     return lista_wartosci
 
