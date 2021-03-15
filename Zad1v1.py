287257904ade267f5c07bb43b929e129e9dba415
Łukasz Gołąbek <luka80471@gmail.com>
2021-03-15 18:43:15 +0100

    Edit_ex1

--- Zad1v1.py
+++ Zad1v1.py
@@ -3,14 +3,12 @@ import random
 
 def bubbleSort(array):
     n = len(array)
-    if_sorted = True
     for i in range(n):
         for j in range(0, n - i - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
-                if_sorted = False
-        if if_sorted:
-            break
+
+
 
 
 array_to_sort = []
