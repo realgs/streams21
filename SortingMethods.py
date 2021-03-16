7a74b19e6d0fe61ad53ed8010416e660e582f783
dariakowal <72565933+dariakowal@users.noreply.github.com>
2021-03-16 16:52:16 +0100

    Dodanie testów

--- SortingMethods.py
+++ SortingMethods.py
@@ -1,3 +1,12 @@
+import random
+import unittest
+
+
+def random_list(k=1000, a=1, b=100):
+    m = [random.randint(a, b) for _ in range(k)]
+    return m
+
+
 def bubble(m):
     for i in range(len(m)):
         for j in range(len(m)-1):
@@ -16,3 +25,18 @@ def insertion(m):
                 m[i] = m[j]
                 m[j] = temp
     return m
+
+
+class SortingMethodsTestCase(unittest.TestCase):
+
+    def test_bubble(self):
+        m = random_list()
+        self.assertEqual(bubble(m), sorted(m))
+
+    def test_insertion(self):
+        m = random_list()
+        self.assertEqual(insertion(m), sorted(m))
+
+
+unittest.main()
+
