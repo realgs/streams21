012b5ac7f68b35312b3eb9a4d779473885aa733a
Karolina Stoga <morze.karolina@gmail.com>
2021-03-17 17:12:44 +0100

    bubble sort modified

--- l1.py
+++ l1.py
@@ -1,15 +1,20 @@
 import random as ran
 
-lista = [ran.randint(0, 3000) for i in range(0, 10)]
 
-def bubble_sort(lista):
-    L = lista
+def createList(x):
+    L = [ran.randint(0, 100) for i in range(0, x)]
+    return L
+
+
+def bubbleSort(L):
     for i in range(len(L)):
         for j in range(0, (len(L) - 1) - i):
-            if (L[j] > L[j + 1]):
+            if L[j] > L[j + 1]:
                 temp = L[j]
                 L[j] = L[j + 1]
                 L[j + 1] = temp
     return L
 
-print(bubble_sort(lista))
+
+print(bubbleSort(createList(15)))
+print(bubbleSort(createList(10)))
