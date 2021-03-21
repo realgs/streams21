dd46313f324f549919211d24ebbaff3251030d3d
Iga-Miller <80620641+Iga-Miller@users.noreply.github.com>
2021-03-21 14:02:05 +0100

    Lista 1

--- Lista1.py
+++ Lista1.py
@@ -1,29 +1,42 @@
 from random import randint
+import unittest
+
+List_to_sort = []
+for i in range(30):
+    List_to_sort.append(randint(1, 1000))
 
 
-def moja_Lista(n):
-    Lista = []
-    for i in range(n):
-        Lista.append(randint(1, 100000))
-    return Lista
+def Bubble_sort(List_to_sort):
+    for i in range(len(List_to_sort)):
+        for j in range(0,(len(List_to_sort))-1):
+            if (List_to_sort[j] > List_to_sort[j+1]):
+                temp = List_to_sort[j]
+                List_to_sort[j] = List_to_sort[j+1]
+                List_to_sort[j+1] = temp
+
+    return List_to_sort
 
 
-def Sortowanie_Babelkowe(Lista):
-    for i in range(len(Lista)):
-        for j in range(0,(len(Lista))-1):
-            if (Lista[j] > Lista[j+1]):
-                temp = Lista[j]
-                Lista[j] = Lista[j+1]
-                Lista[j+1] = temp
-
-    return Lista
-
-
-def Sortowanie_wstawianie(Lista):
-    for i in range(len(Lista)):
+def Insert_sort(List_to_sort):
+    for i in range(len(List_to_sort)):
         for j in range(0, i):
-            if (Lista[i] < Lista[j]):
-                temp = Lista[i]
-                Lista[i] = Lista[j]
-                Lista[j] = temp
-    return Lista
+            if (List_to_sort[i] < List_to_sort[j]):
+                temp = List_to_sort[i]
+                List_to_sort[i] = List_to_sort[j]
+                List_to_sort[j] = temp
+    return List_to_sort
+
+sorted_bubble=Bubble_sort(List_to_sort)
+sorted_insert=Insert_sort(List_to_sort)
+print(sorted_bubble)
+print(sorted_insert)
+
+
+class SortingMethodsTest(unittest.TestCase):
+    def Bubble_sort_test(self):
+        self.assertEqual(Bubble_sort(List_to_sort), sorted(List_to_sort))
+    def Insert_sort_test(self):
+        self.assertEqual(Insert_sort(List_to_sort), sorted(List_to_sort))
+
+
+unittest.main()
