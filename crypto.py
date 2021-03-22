a2a69df13aa8ce9252fd927987b1319c0d7ad1ae
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2021-03-22 19:57:26 +0100

    Error handling

--- crypto.py
+++ crypto.py
@@ -4,11 +4,12 @@ import time
 
 
 def getPath(currency, category):
+  # return 'https://bitbay.net/API/Public/THISISWRONG'
   return f'https://bitbay.net/API/Public/{currency}/{category}.json'
 
 
 def printOffers(offers, title='Offers'):
-  print(f'\n{title}:')
+  print(f'{title}:')
   if len(offers) > 6:  # shorten the list
     offers = offers[:3]+['...']+offers[-3:]
   for offer in offers:
@@ -17,25 +18,39 @@ def printOffers(offers, title='Offers'):
 def fetchOffers(currency):
   API = getPath(currency, 'orderbook')
   req = requests.get(API)
-  json = req.json()
-  return (json['bids'], json['asks'])
+  if req.status_code == 200:
+    json = req.json()
+    return (json['bids'], json['asks'])
+  else:
+    print('Error: Connection failed')
+    return ()
+
 
 def fetchAndPrintOffers(currency):
-  bids, asks = fetchOffers(currency)
-  printOffers(bids, f'{currency} Bids')
-  printOffers(asks, f'{currency} Asks')
-
+  print('\nFetching data from an external API... ')
+  offers = fetchOffers(currency)
+  if len(offers):
+    bids, asks = offers
+    printOffers(bids, f'{currency} Bids')
+    printOffers(asks, f'{currency} Asks')
+  else:
+    print('No data')
 
 def startStream(currency, frequency):
   while 1:
-    bids, asks = fetchOffers(currency)
-    last_bid_rate = bids[0][0]
-    last_ask_rate = asks[0][0]
-    stat = 1 - (last_ask_rate - last_bid_rate) / last_bid_rate
-    print(f'Percentage: {stat}')
-    stream.append(stat)
-    print(f'Stream: {stream}')
-    print(f'Sleeping for {frequency}s...')
+    print('\nFetching data from an external API... ')
+    offers = fetchOffers(currency)
+    if len(offers):
+      bids, asks = offers
+      last_bid_rate = bids[0][0]
+      last_ask_rate = asks[0][0]
+      stat = 1 - (last_ask_rate - last_bid_rate) / last_bid_rate
+      print(f'Percentage: {stat}')
+      stream.append(stat)
+      print(f'Stream: {stream}')
+      print(f'Sleeping for {frequency}s...')
+    else:
+      print('No data. Trying again...')
     time.sleep(frequency)
 
 
