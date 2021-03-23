d5ae1d50584151bcc69d997acc16f12bc7825112
jankrzempek <jankrzempek9@gmail.com>
2021-03-23 22:13:20 +0100

    added statu_code handling

--- RESTAPI.py
+++ RESTAPI.py
@@ -1,17 +1,24 @@
 import requests
 import time
+import sys
 
 def calculate(BuyPrice, sellPrice):
     output = round((1 - (sellPrice - BuyPrice) / BuyPrice), 3)
     print(str(output) + " %")
 
 def getCurrencyData(currency, category):
-    print(f"-----------{currency}-------------")
     url = f"https://bitbay.net/API/Public/{currency}/{category}.json"
-    response = requests.get(url).json()
-    sellPrice = response['ask']
-    buyPrice = response['bid']
-    calculate(buyPrice, sellPrice)
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"-----------{currency}-------------")
+        json = response.json()
+        response = json
+        sellPrice = response['ask']
+        buyPrice = response['bid']
+        calculate(buyPrice, sellPrice)
+    else:
+        print("Error when trying to fetch")
+        sys.exit()
 
 if __name__ == "__main__":
     while(True):
@@ -20,4 +27,4 @@ if __name__ == "__main__":
         getCurrencyData("ETH", "ticker")
         getCurrencyData("ZEC", "ticker")
         time.sleep(5)
-        
+
