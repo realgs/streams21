b7074d7877881762b046694307b1bd7373a89c4d
Bartosz-Stec <71563907+Bartosz-Stec@users.noreply.github.com>
2021-03-23 14:47:54 +0100

    Connecting with http

--- L2.1.py
+++ L2.1.py
@@ -2,4 +2,14 @@ import requests
 from requests.exceptions import HTTPError
 import time
 
+def krypto_market(data):
+    try:
+        response = requests.get('http://bitbay.net/API/Public/' + data + 'USD/ticker.json')
+        response.raise_for_status()
+    except HTTPError as error_with_http:
+        print(f'Błąd z adresem HTTP : {error_with_http}')
 
+
+while True:
+    krypto_market('BTC')
+    time.sleep(10)
