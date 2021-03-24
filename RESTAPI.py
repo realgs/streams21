4e4bbd9d65bb3e3a816fad341d78378fae361bc3
jankrzempek <jankrzempek9@gmail.com>
2021-03-24 17:17:42 +0100

    Fixes

--- RESTAPI.py
+++ RESTAPI.py
@@ -1,21 +1,30 @@
 import requests
 import time
 import sys
+import urllib.request
 
-def calculate(BuyPrice, sellPrice):
-    output = round((1 - (sellPrice - BuyPrice) / BuyPrice), 3)
+def calculate(buy_price, sell_price):
+    output = round((1 - (sell_price - buy_price) / buy_price), 3)
     print(str(output) + " %")
 
+def connect():
+    try:
+        urllib.request.urlopen('http://google.com')
+    except:
+        print("No internet connection")
+        sys.exit()
+
 def getCurrencyData(currency, category):
+    connect()
     url = f"https://bitbay.net/API/Public/{currency}/{category}.json"
     response = requests.get(url)
     if response.status_code == 200:
         print(f"-----------{currency}-------------")
         json = response.json()
         response = json
-        sellPrice = response['ask']
-        buyPrice = response['bid']
-        calculate(buyPrice, sellPrice)
+        sell_price = response['ask']
+        buy_price = response['bid']
+        calculate(buy_price, sell_price)
     else:
         print("Error when trying to fetch")
         sys.exit()
@@ -27,4 +36,4 @@ if __name__ == "__main__":
         getCurrencyData("ETH", "ticker")
         getCurrencyData("ZEC", "ticker")
         time.sleep(5)
-
+        
