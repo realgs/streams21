c26e49736a0e04950a59f2a5964b762e28c03139
Paul <pawelpelar@gmail.com>
2021-03-24 13:12:32 +0100

    Function to handle 2 url

--- L2/main.py
+++ L2/main.py
@@ -1,15 +1,29 @@
 import requests
 import time
+from requests.exceptions import HTTPError
+import json
 
-def down_n_print(url):
-    status_code = 200
-    while status_code == 200:
-        a = requests.get(url)
-        print(a.content)
-        time.sleep(10)
+def down_n_print(url1,url2):
+
+    for url in [url1, url2]:
+        try:
+            response = requests.get(url)
+            print(response.json())
+            print(type(response.json()))
+
+            # If the response was successful, no Exception will be raised
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # Python 3.6
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+        else:
+            print('Success!')
 
 
-url = "https://bitbay.net/API/Public/BTC/trades.json"
 
+url1 = "https://bitbay.net/API/Public/BTC/trades.json"
+url2 = "https://btbay.net/API/Public/BTC/trades.json"
 
-down_n_print(url)
+down_n_print(url1,url2)
+
