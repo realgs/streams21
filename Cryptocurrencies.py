5ee41e583817b17da9531c1db10fc1b257d24775
Piterekg9 <piterekg9@wp.pl>
2021-03-26 18:36:50 +0100

    Error handling

--- Cryptocurrencies.py
+++ Cryptocurrencies.py
@@ -1,14 +1,6 @@
 import requests
 import sys
-import time
 
-def Price_diff(buy_price,sell_price):
-    return round(100*(1-sell_price/buy_price),2)
-
-def Print_data(Currency,c_buy_price,c_sell_price):
-    print(f' BUY  SELL    Currency: {Currency}')
-    print(c_buy_price, c_sell_price)
-    print(f'Difference in prices {Price_diff(c_buy_price, c_sell_price)}%' + '\n')
 
 def Download_data(c_currencies,Category): #c_currencies is a list
     for Currency in c_currencies:
@@ -18,12 +10,10 @@ def Download_data(c_currencies,Category): #c_currencies is a list
             data = requests.get(url).json()
             c_buy_price = data['ask']
             c_sell_price = data['bid']
-            Print_data(Currency,c_buy_price,c_sell_price)
+            print(c_buy_price,c_sell_price)
         else:
             print("Could not download data. Try again later!")
             sys.exit()
-    time.sleep(5)
-    Download_data(c_currencies,Category)
-
 
 Download_data(['BTCUSD','LTCUSD' ,'DASHUSD'],'ticker')
+
