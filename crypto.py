2ec3499d8013307f55b46424fe6982e1f2fa3058
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2021-03-27 17:19:22 +0100

    format code

--- crypto.py
+++ crypto.py
@@ -14,23 +14,23 @@ def crypto(currencies, category):  # 1
         result.append([currency, buy_price, sell_price])
     return result
 
-def calculate(buy_price,sell_price):
-    return round(100*(1-sell_price/buy_price),2)
+
+def calculate(buy_price, sell_price):
+    return round(100*(1-sell_price/buy_price), 2)
+
 
 def refreshing_results(currencies, category):  # 2
     while(True):
-        data = crypto(currencies,category)
-        print(f'BTCUSD % diffrence between sell and buy price: {calculate(data[0][1],data[0][2])}')
-        print(f'LTCUSD % diffrence between sell and buy price: {calculate(data[1][1],data[1][2])}')
-        print(f'DASHUSD % diffrence between sell and buy price: {calculate(data[2][1],data[2][2])}')
+        data = crypto(currencies, category)
+        print(
+            f'BTCUSD % diffrence between sell and buy price: {calculate(data[0][1],data[0][2])}')
+        print(
+            f'LTCUSD % diffrence between sell and buy price: {calculate(data[1][1],data[1][2])}')
+        print(
+            f'DASHUSD % diffrence between sell and buy price: {calculate(data[2][1],data[2][2])}')
         time.sleep(5)
-
-
-
-
-
 
 
 currencies = ['BTCUSD', 'LTCUSD', 'DASHUSD']
 category = 'ticker'
-refreshing_results(currencies,category)
+refreshing_results(currencies, category)
