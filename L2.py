adab04dc12a2387e0ce5814e7f0d43fd772656af
dariakowal <72565933+dariakowal@users.noreply.github.com>
2021-03-27 17:32:07 +0100

    adding all functions

--- L2.py
+++ L2.py
@@ -1,1 +1,40 @@
-print([1, 2, 3])
+import requests
+import pprint
+import time
+
+urls = {'BTCUSD': 'https://bitbay.net/API/Public/BTCUSD/orderbook.json',
+        'LTCUSD': 'https://bitbay.net/API/Public/LTCUSD/orderbook.json',
+        'DASHUSD': 'https://bitbay.net/API/Public/DASHUSD/orderbook.json'}
+
+
+def orderbook(url):
+    for key in url.keys():
+        try:
+            r = requests.get(url[key])
+            r.raise_for_status()
+            print(f'{key} -----------------------------------')
+            # pprint.pprint(r.json())
+            print(r.json())
+
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
+
+
+def data_stream(url):
+    while True:
+        for key in url.keys():
+            try:
+                r = requests.get(url[key])
+                r.raise_for_status()
+                result = 1 - (r.json()['asks'][0][0] - r.json()['bids'][0][0]) / r.json()['bids'][0][0]
+                print(f'{key}: ', round(result, 4))
+
+            except requests.exceptions.HTTPError as err:
+                raise SystemExit(err)
+
+        print('\n')
+        time.sleep(5)
+
+
+orderbook(urls)
+data_stream(urls)
