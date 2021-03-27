d989949f745dc1e0a0a4d1a971f8b17ebb12e7f4
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2021-03-27 16:56:34 +0100

    create crypto function

--- crypto.py
+++ crypto.py
@@ -2,14 +2,18 @@ import requests as r
 import time
 
 
+def crypto(currencies, category):
+    result = []
+    for currency in currencies:
+        URL = f'https://bitbay.net/API/Public/{currency}/{category}.json'
+        request = r.get(URL).json()
+        buy_price = request['ask']
+        sell_price = request['bid']
+        print(f'Currency: {currency} Sell price: {sell_price} Buy price: {buy_price}')
+        result.append([currency,buy_price,sell_price])
+    return result
+    
 
-
-
-
-
-
-
-
-currencies = ['BTCUSD','LTCUSD' ,'DASHUSD']
+currencies = ['BTCUSD', 'LTCUSD', 'DASHUSD']
 category = 'ticker'
-crypto(currencies,category)
+crypto(currencies, category)
