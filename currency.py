db2092fd6d8573d7d4a8d0aec49518d69db09af1
PKozicki <p.kozicki@windowslive.com>
2021-03-27 22:13:58 +0100

    l2 completed

--- currency.py
+++ currency.py
@@ -1,41 +1,34 @@
 import requests
-import time
-import datetime
+import pprint
 
 
-def get_data(url):
-    response = requests.get(url)
-    return response.json()
+def get_info(url_list, pairs_list, printing=True):
+    result = list()
+    for i in range(len(url_list)):
+        try:
+            response = requests.get(url_list[i])
+        except requests.exceptions.ConnectionError:
+            print("Connection error has occurred")
+            return None
+        result.append(response.json())
 
+    if printing:
+        for i in range(len(result)):
+            print("*" * 25, pairs_list[i], "*" * 25)
+            pprint.pprint(result[i])
+            print("\n\n\n")
 
-def collect_data(urls):
-    # in case you want to save it in the future
-    currency_info = list()
-    for url in urls:
-        data = get_data(url)
-        data_info = (list(data['rates'].keys())[0],
-                     data['rates'][list(data['rates'].keys())[0]]['rate'],
-                     data['rates'][list(data['rates'].keys())[0]]['timestamp'])
-        currency_info.append(data_info)
-
-    return currency_info
-
-
-def print_info(frequency, repeats, urls):
-    for _ in range(repeats):
-        currency_info = collect_data(urls)
-        print('*' * 14, datetime.datetime.now().strftime("%d.%m.%Y, %H:%M:%S"), '*' * 14)
-        for currency in currency_info:
-            print(f"{currency[0]}: {currency[1]} \t\t\ttimestamp: {currency[2]}")
-        print()
-        time.sleep(frequency)
+    return result
 
 
 if __name__ == "__main__":
-    URL_LIST = ['https://www.freeforexapi.com/api/live?pairs=USDPLN',
-                'https://www.freeforexapi.com/api/live?pairs=USDEUR',
-                'https://www.freeforexapi.com/api/live?pairs=USDGBP']
-    FREQUENCY = 5 # seconds
-    REPEATS = 10
+    URL = "https://api.bittrex.com/v3/markets/{}/orderbook"
+    PAIRS = ['BTC-USD',
+             'ETH-USD',
+             'LTC-USD']
 
-    print_info(FREQUENCY, REPEATS, URL_LIST)
+    URLS = list()
+    for i in range(len(PAIRS)):
+        URLS.append(URL.format(PAIRS[i]))
+
+    get_info(URLS, PAIRS)
