89583906e73b9f3565d3323661ab29447de78ad9
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2021-03-27 17:18:51 +0100

    add calculate function

--- crypto.py
+++ crypto.py
@@ -2,18 +2,29 @@ import requests as r
 import time
 
 
-def crypto(currencies, category):
+def crypto(currencies, category):  # 1
     result = []
     for currency in currencies:
         URL = f'https://bitbay.net/API/Public/{currency}/{category}.json'
         request = r.get(URL).json()
         buy_price = request['ask']
         sell_price = request['bid']
-        print(f'Currency: {currency} Sell price: {sell_price} Buy price: {buy_price}')
-        result.append([currency,buy_price,sell_price])
+        print(
+            f'Currency: {currency} Sell price: {sell_price} Buy price: {buy_price}')
+        result.append([currency, buy_price, sell_price])
     return result
-    
+
+def calculate(buy_price,sell_price):
+    return round(100*(1-sell_price/buy_price),2)
+
+
+
+
+
+
+
+
 
 currencies = ['BTCUSD', 'LTCUSD', 'DASHUSD']
 category = 'ticker'
-crypto(currencies, category)
+refreshing_results(currencies,category)
