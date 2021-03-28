c2651ec1bf82347c7a6a752a665e1b542518efee
AKulakowska <254311@student.pwr.edu.pl>
2021-03-28 23:58:43 +0200

    added buy offer method

--- lista 2/lista2.py
+++ lista 2/lista2.py
@@ -10,13 +10,29 @@ def sell_offer (fromTo, N):
         print(response['sell'][i]['ra'])
         i += 1
 
+def buy_offer (fromTo, N):
+    url = f"https://api.bitbay.net/rest/trading/orderbook/{fromTo}"
+    headers = {'content-type': 'application/json'}
+    response = requests.request("GET", url, headers=headers).json()
+
+    i = 0
+    while i < N:
+        print(response['buy'][i]['ra'])
+        i += 1
+
 def main():
+    print("Sprzedaż:")
     sell_offer("BTC-USD", 3)
-    print("=========")
+    print("Kupno:")
+    buy_offer("BTC-USD", 3)
+    print("Sprzedaż:")
     sell_offer("LTC-USD", 10)
-    print("=========")
+    print("Kupno:")
+    buy_offer("LTC-USD", 10)
+    print("Sprzedaż:")
     sell_offer("DASH-USD", 5)
-    print("=========")
+    print("Kupno:")
+    buy_offer("DASH-USD", 5)
 
 
 if __name__ == '__main__':
