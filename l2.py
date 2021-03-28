6048d2e10c6aa648dc437cd18e84b8f8e9147456
Ola <olek@rafkos.net>
2021-03-28 18:01:27 +0200

    get_data function

--- l2.py
+++ l2.py
@@ -1,3 +1,5 @@
+import requests
+import sys
 
 
 def purchase_selling_diff(purchase_cost, selling_cost):
@@ -5,4 +7,22 @@ def purchase_selling_diff(purchase_cost, selling_cost):
     return '{}{}'.format(result, " %")
 
 
-print(purchase_selling_diff(2, 1))
+def get_data(currency):
+    url = 'https://bitbay.net/API/Public/{}/ticker.json'.format(currency)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            resp_json = response.json()
+            selling_cost = resp_json['ask']
+            purchase_cost = resp_json['bid']
+            print('{}: Cena kupna: {}, cena sprzedaży: {}, wynik procentowy:{}'
+                  .format(currency, purchase_cost, selling_cost, purchase_selling_diff(purchase_cost, selling_cost)))
+        else:
+            print("Unable to access API on BitBay. Error: {}".format(response.status_code))
+            sys.exit()
+    except requests.exceptions.ConnectionError:
+        print("Cannot reach the server.")
+        sys.exit()
+
+
+get_data('BTC')
