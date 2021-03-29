ddb8ef7c065bf059ece8f58e652d7ea56dcd474e
Bartosz-Stec <71563907+Bartosz-Stec@users.noreply.github.com>
2021-03-29 17:56:01 +0200

    Adding const values

--- L2.1.py
+++ L2.1.py
@@ -3,25 +3,17 @@ from requests.exceptions import HTTPError
 import time
 from datetime import datetime
 
+url_start='http://bitbay.net/API/Public/'
+url_end='USD/ticker.json'
+interval=10
 def krypto_market(data):
     try:
-        response = requests.get('http://bitbay.net/API/Public/' + data + 'USD/ticker.json')
+        response = requests.get( url_start + data + url_end)
         response.raise_for_status()
 
     except HTTPError as error_with_http:
         print(f'Błąd z adresem HTTP : {error_with_http}')
 
-    # else:
-    #     originalData = response.content.decode()
-    #     Cooked_Data = {}
-    #     for elem in originalData[slice(1,-1)].split(","):
-    #         part = elem.split(":")
-    #         key = part[0]
-    #         Cooked_Data[key[slice(1,-1)]] = part[1]
-    #     value =(1-(float(Cooked_Data["ask"]) - float(Cooked_Data["bid"])) / float(Cooked_Data["bid"])*100)
-    #     # print(f'Request status: {response.status_code}')
-    #     print(f'{data} : Różnica procentowa pomiędzy ceną kupna, a sprzedarzy {value}%')
-    #     return value
     else:
         originalData = response.json()
         value = (100 - (1 - (float(originalData["ask"]) - float(originalData["bid"])) / float(originalData["bid"])) * 100 )
@@ -40,4 +32,4 @@ while True:
     krypto_market('LTC')
     print('____________________________________________')
 
-    time.sleep(10)
+    time.sleep(interval)
