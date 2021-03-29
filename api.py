3eb2d826e3d4d24759ff64319b4a7ae14ea1e4cb
swojciechowski7 <slawek.woj2000@gmail.com>
2021-03-29 13:18:10 +0200

    Connecting to API

--- api.py
+++ api.py
@@ -1,4 +1,18 @@
+import requests
+import sys
+
 def path(currency):
     url = f"https://bitbay.net/API/Public/{currency}/orderbook.json"
     return url
+
+def connect(currency):
+    req = requests.get(path(currency))
+    if req.status_code == 200:
+        json = req.json()
+        print("Bids:", json['bids'])
+        print("Asks:", json['asks'])
+
+    else:
+        print("Failed to connect, try again")
+        sys.exit()
 
