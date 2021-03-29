62eb8c6d19babf89974288cdbe6ddc2f0f88a69b
blaszczykk <blaszczykkarolpawel@gmail.com>
2021-03-29 17:11:06 +0200

    added difference calculator

--- finapi.py
+++ finapi.py
@@ -6,15 +6,12 @@ import time
 PAIRS = [('BTC','USD'),('ETH','USD'),('BCC','USD')]
 
 
-def print_orders(first_currency, second_currency):
-
+def get_orders(first_currency, second_currency):
   try:
     request = requests.get(
       f"https://bitbay.net/API/Public/{first_currency}{second_currency}/orderbook.json"
     )
     orders = request.json()
-    print("Bid orders\n", orders['bids'])
-    print("Ask orders\n", orders['asks'])
 
   except requests.exceptions.RequestException:
     print("Connection problem.")
@@ -23,5 +20,15 @@ def print_orders(first_currency, second_currency):
   return orders
 
 
-for i in PAIRS:
-  print_orders(PAIRS[i][0],PAIRS[i][1])
+def print_orders(orders):
+  print("Bid orders\n", orders['bids'])
+  print("Ask orders\n", orders['asks'])
+
+
+def calculate_difference(ask_price, bid_price):
+  return (1 - (ask_price - bid_price) / bid_price) * 100
+
+
+for pair in PAIRS:
+  print_orders(get_orders(pair[0],pair[1]))
+  time.sleep(5)
