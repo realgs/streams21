4dc59781c1aebf8fd42b4ac20cce2464a4899689
swojciechowski7 <slawek.woj2000@gmail.com>
2021-03-29 13:19:14 +0200

    Constant connect and counting difference

--- api.py
+++ api.py
@@ -1,9 +1,17 @@
 import requests
 import sys
+import time
+
 
 def path(currency):
     url = f"https://bitbay.net/API/Public/{currency}/orderbook.json"
     return url
+
+
+def count(bid, ask):
+    diff = round((1 - (ask - bid)) / bid, 4)
+    return diff
+
 
 def connect(currency):
     req = requests.get(path(currency))
@@ -15,4 +23,16 @@ def connect(currency):
     else:
         print("Failed to connect, try again")
         sys.exit()
+
+
+def refresh_connect(currency):
+    req = requests.get(path(currency))
+    connect(currency)
+    json = req.json()
+    bid = json['bids'][0][0]
+    ask = json['asks'][0][0]
+    sol = count(bid, ask)
+    print(f"Difference between bid and ask: {sol}%")
+    time.sleep(5)
+    refresh_connect(currency)
 
