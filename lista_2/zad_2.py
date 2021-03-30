dd1564ab68e8056e0e3f80cd96053ce381807e4a
Michal Chylak <michauchylak@gmail.com>
2021-03-30 22:32:03 +0200

    dodana funkcja kalkulatora

--- lista_2/zad_2.py
+++ lista_2/zad_2.py
@@ -11,22 +11,24 @@ ADRESS = {
 def market():
     counter = 1
     while True:
-        print(counter, "Buy - sell difference:")
+        print(counter, "survey of buy - sell difference:")
         for key in ADRESS.keys():
             try:
                 request = requests.get(ADRESS[key])
                 if request.status_code == 200:
                     bids = request.json()['bids'][20][0]
                     asks = request.json()['asks'][20][0]
-                    difference = 1 - (asks - bids)/bids
-                    print(difference)
+                    print(key,'-',calc(bids, asks),"%")
                 else:
                     print('ERROR')
             except HTTPError:
                 print('ERROR: ',HTTPError)
         counter += 1
         print('------------------')
-        time.sleep(5)
-        
+        time.sleep(20)
+
+
+def calc(bids, asks):
+    return round(((1 - (asks - bids)/bids)*100),2)
 
 market()
