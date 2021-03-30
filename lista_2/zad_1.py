3243eabad54b273c5593aec65a3f71ed4a2fb1d0
Michal Chylak <michauchylak@gmail.com>
2021-03-30 23:03:51 +0200

    zadanie 1 final

--- lista_2/zad_1.py
+++ lista_2/zad_1.py
@@ -2,9 +2,9 @@ import requests
 from requests.exceptions import HTTPError
 
 ADRESS = {
-    'BTC-PLN' : 'https://bitbay.net/API/Public/BTCPLN/ticker.json',
-    'LTC-PLN' : 'https://bitbay.net/API/Public/LTCPLN/ticker.json',
-    'ZRX-PLN' : 'https://bitbay.net/API/Public/DASHPLN/ticker.json'
+    'BTC-PLN' : 'https://bitbay.net/API/Public/BTCPLN/market.json',
+    'LTC-PLN' : 'https://bitbay.net/API/Public/LTCPLN/market.json',
+    'ZRX-PLN' : 'https://bitbay.net/API/Public/ZRXPLN/market.json'
 }
 
 def my_requests():
@@ -12,7 +12,7 @@ def my_requests():
         try:
             request = requests.get(ADRESS[key])
             if request.status_code == 200:
-                print(key, request.json(),'\nLast:',request.json()['last'],'\nAsk:',request.json()['ask'],'\n')
+                print(key, request.json()['transactions'][15])
             else:
                 print('Error in ', key, ' request')
         except HTTPError:
