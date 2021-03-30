bf48bcc75da886fd1f03678edbab883fcbb06380
Michal Chylak <michauchylak@gmail.com>
2021-03-30 18:44:12 +0200

    odczyt danych z serwera

--- lista_2/main.py
+++ lista_2/main.py
@@ -1,6 +1,5 @@
 import requests
-import requests.exceptions
-import time
+from requests.exceptions import HTTPError
 
 ADRESS = {
     'btcpln' : 'https://bitbay.net/API/Public/BTCPLN/orderbook.json',
@@ -8,4 +7,15 @@ ADRESS = {
     'dashusd': 'https://bitbay.net/API/Public/DASHPLN/orderbook.json'
 }
 
-def market():
+def my_requests():
+    for key in ADRESS.keys():
+        try:
+            request = requests.get(ADRESS[key])
+            if request.status_code == 200:
+                print(key,request.json(),'\n')
+            else:
+                print('Error in ', key, ' request')
+        except HTTPError:
+            print('Error: ', HTTPError)
+
+my_requests()
