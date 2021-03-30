ffcde9b5ac1f64e7b8341e2c8c7f49d09bb56f48
Michal Chylak <michauchylak@gmail.com>
2021-03-30 18:02:54 +0200

    import bibliotek i ustalenie stałych adresów

--- lista_2/main.py
+++ lista_2/main.py
@@ -1,1 +1,11 @@
-print('super działa!')
+import requests
+import requests.exceptions
+import time
+
+ADRESS = {
+    'btcpln' : 'https://bitbay.net/API/Public/BTCPLN/orderbook.json',
+    'ltcusd' : 'https://bitbay.net/API/Public/LTCPLN/orderbook.json',
+    'dashusd': 'https://bitbay.net/API/Public/DASHPLN/orderbook.json'
+}
+
+def market():
