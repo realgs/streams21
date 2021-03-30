b9303cbd3ae2ece42531dd7a92a497ae5758e0dd
ibockowsky github <ibockowskt@gmail.com>
2021-03-30 15:11:08 +0200

    feat: mock-up data

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
-
+	import Currency from '$lib/components/Currency.svelte'
 </script>
 
 <main>
-	
+	<Currency cryptoCurrencyCode="BTC" realCurrencyCode="USD" />
+	<Currency cryptoCurrencyCode="ETH" realCurrencyCode="USD" />
+	<Currency cryptoCurrencyCode="XLM" realCurrencyCode="USD" />
 </main>
