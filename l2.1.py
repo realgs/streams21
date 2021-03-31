22692afbd971525243aa760d0f7323616fc3bb10
non4me <kacper.w21@wp.pl>
2021-03-31 20:23:27 +0200

    Deleted repeated code, changed functions name, added time interval

--- l2.1.py
+++ l2.1.py
@@ -2,16 +2,21 @@ import requests
 
 
 currency = ['BTCPLN', 'ETHPLN', 'LSKPLN']
-urls = [f'https://bitbay.net/API/Public/{currency[0]}/ticker.json',f'https://bitbay.net/API/Public/{currency[1]}/ticker.json',f'https://bitbay.net/API/Public/{currency[2]}/ticker.json']
 
-def get_data(url):
-    response = requests.get(url)
-    data = response.json()
-    return data
-def show_ratio():
-    for i in range(0,len(urls)):
-        data = get_data(urls[i])
+def get_data(currency):
+    try:
+        response = requests.get(f'https://bitbay.net/API/Public/{currency}/ticker.json')
+        data = response.json()
+        return data
+    except requests.exceptions.MissingSchema:
+        print("Missing URL schema (e.g. http or https)")
+    except requests.exceptions.ConnectionError:
+        print("Connection Error occured")
+
+def show_rates():
+    for i in range(0,len(currency)):
+        data = get_data(currency[i])
         print(f'Currency: {currency[i]} \n Bid: {data["bid"]} Ask: {data["ask"]}')
 
 
-show_ratio()
+show_rates()
--- l2.2.py
+++ l2.2.py
@@ -4,23 +4,31 @@ from numpy import round
 
 
 currency = ['BTCPLN', 'ETHPLN', 'LSKPLN']
-urls = [f'https://bitbay.net/API/Public/{currency[0]}/ticker.json',f'https://bitbay.net/API/Public/{currency[1]}/ticker.json',f'https://bitbay.net/API/Public/{currency[2]}/ticker.json']
 
 
-def get_data(url):
-    response = requests.get(url)
-    data = response.json()
-    return data
+def get_data(currency):
+    try:
+        response = requests.get(f'https://bitbay.net/API/Public/{currency}/ticker.json')
+        data = response.json()
+        return data
+    except requests.exceptions.MissingSchema:
+        print("Missing URL schema (e.g. http or https)")
+    except requests.exceptions.ConnectionError:
+        print("Connection Error occured")
 
-def calculate(url):
-    data = get_data(url)
+
+def calculate_spread(currency):
+    data = get_data(currency)
     return ((data['ask'] - data['bid']) / data['bid']) * 100
 
-def show_result():
+def show_result(time_interval):
     while True:
-        for i in range (0,len(urls)):
-            print(f'For {currency[i]} difference between selling and buying is {round(calculate(urls[i]),5)}% ')
+        for i in range (0,len(currency)):
+            print(f'For {currency[i]} difference between selling and buying is {round(calculate_spread(currency[i]),5)}% ')
         print("------------------------")
-        sleep(5)
+        sleep(time_interval)
 
-show_result()
+try:
+    show_result(5)
+except KeyboardInterrupt:
+    print("End of work")
