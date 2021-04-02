643d3d821ccb98b384b8e6dabcc67c84f2cd0da6
ibockowsky github <ibockowskt@gmail.com>
2021-04-02 20:05:43 +0200

    refactor: create own table component

---
+++ app/src/lib/components/CurrencyHistory.svelte
@@ -0,0 +1,54 @@
+<script>
+    export let currencyData
+</script>
+
+<style lang="scss">
+    .table-history {
+        position: relative;
+        &__header, &__body {
+            &--row {
+                height: 30px;
+                display: flex;
+                flex-direction: row;
+            }
+
+            &--cell {
+                width: 80px;
+            }
+        }
+
+        &__body {
+            padding-top: 30px;
+            position: absolute;
+
+            &--scroll {
+                display: flex;
+                flex-direction: column;
+                overflow-y: auto;
+                max-height: 90px;
+            }
+        }
+    }
+</style>
+
+<div class="table-history">
+    {#if currencyData && currencyData.length > 0}
+    <div class="table-history__header"></div>
+        <div class="table-history__header--row">
+            <div class="table-history__header--cell"><span>ask</span></div>
+            <div class="table-history__header--cell"><span>bid</span></div>
+            <div class="table-history__header--cell"><span>difference</span></div>
+        </div>
+    <div class="table-history__body">
+        <div class="table-history__body--scroll">
+            {#each currencyData as { ask, bid, difference}}
+            <div class="table-history__body--row">
+                <div class="table-history__body--cell"><span>{ ask }</span></div>
+                <div class="table-history__body--cell"><span>{ bid }</span></div>
+                <div class="table-history__body--cell"><span>{ difference } %</span></div>
+            </div>
+            {/each}
+        </div>
+    </div>
+    {/if}
+</div>
--- app/src/lib/components/CurrencyPlot.svelte
+++ app/src/lib/components/CurrencyPlot.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from 'svelte'
     import { handleData } from '$lib/services/FinanceApi'
+    import Currencyhistory from '$lib/components/CurrencyHistory.svelte'
 
     export let cryptoCurrencyCode
     export let realCurrencyCode
@@ -18,53 +19,8 @@
 </script>
 
 <style lang="scss">
-    .wrapper {
-        width: 33.33%;
-        margin: 0 auto;
-    }
-
-    .currency-table {
-        width: 100%;
-        position: relative;
-        display: inline-block;
-
-        th, td {
-            max-width: 33.33%;
-            text-align: left;
-        }
-
-        thead, tbody {
-            width: 100%;
-        }
-
-        &__body {
-            overflow: auto;
-            max-height: 150px;
-        }
-    }
 </style>
 
-<div class="wrapper">
-    {#if downloadedData && downloadedData.length > 0}
-    <table class="currency-table">
-        <thead>
-            <tr>
-                <th>Ask</th>
-                <th>Bid</th>
-                <th>Difference</th>
-            </tr>
-        </thead>
-        <div class="currency-table__body">
-            <tbody>
-                    {#each downloadedData as { ask, bid, difference }}
-                    <tr>
-                        <td>{ ask } </td>
-                        <td>{ bid } </td>
-                        <td>{ difference } </td>
-                    </tr>
-                    {/each}
-            </tbody>
-        </div>
-    </table>
-    {/if}
+<div>
+    <Currencyhistory currencyData="{ downloadedData }" />
 </div>
