c9b1ad5dcbcf7ce27e7c7343f87e94f785b5e341
ibockowsky github <ibockowskt@gmail.com>
2021-04-03 12:34:11 +0200

    chore: implement vue framework

---
+++ app/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:vue/essential',
+    '@vue/prettier'
+  ],
+  rules: {
+    'import/no-unresolved': 0,
+    'import/no-unassigned-import': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
