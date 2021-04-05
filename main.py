a8b74c6c7f9e615b8d42948fca415863f6507715
dkosko <dimasikeagle@gmail.com>
2021-04-05 13:09:19 +0300

    devided_functions

---
+++ main.py
@@ -0,0 +1,63 @@
+from math import inf
+import requests
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+from itertools import count
+
+
+def get_data(crypt, since):
+    url = "https://bitbay.net/API/Public/{Currency[0]}{Currency[1]}/{Category}.json?since={since}".format(Currency=[crypt, 'USD'],
+                                                                                            Category='trades', since=since)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+         print("Can't get data from API \n status code: ", response.status_code)
+
+
+def count_minmax(data):
+    max_sell = -inf
+    min_buy = inf
+    for trade in data:
+        if trade['type'] == 'sell':
+            if max_sell < trade['price']:
+                max_sell = trade['price']
+        elif trade['type'] == 'buy':
+            if min_buy > trade['price']:
+                min_buy = trade['price']
+
+    return max_sell, min_buy
+
+def data_stream(crypt, x, y_1, y_2, i):
+    x.append(i)
+    data = get_data(crypt, since= 50*i)
+    max, min = count_minmax(data)
+    y_1.append(min)
+    y_2.append(max)
+    return [x, y_1, y_2]
+
+def plot_data(x, y_1, y_2):
+    pass
+
+index = count()
+x = []
+y_1 = []
+y_2 = []
+# for i in range(5):
+#     x, y_1, y_2 = data_stream('DASH', x, y_1, y_2, i)
+#     #print(x, y_1, y_2)
+#     plt.plot(x, y_1)
+
+def animate(i):
+    #x.append(next(index))
+    temp = data_stream('DASH', x, y_1, y_2, i)
+    x.append(temp[0])
+    y_1.append(temp[1])
+    plt.plot(x, y_1)
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval= 1000)
+
+plt.tight_layout()
+plt.show()
