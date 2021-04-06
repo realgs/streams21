9f25d7fa76c1e2fa717ab3316ba56fa119ac7d87
Paul <pawelpelar@gmail.com>
2021-04-06 18:27:27 +0200

    Refactor, more usibility

--- L2/main.py
+++ L2/main.py
@@ -2,35 +2,42 @@ import requests
 import time
 from requests.exceptions import HTTPError
 
-def get_data(url1,url2,url3):
+class Finance():
 
-    content = []
-    for url in [url1, url2,url3]:
+    def __init__(self):
+        self.currencies = ['BTC','GNT','DASH']
+        self.pre_url = 'https://bitbay.net/API/Public/'
+        self.post_url = '/ticker.json'
+
+    def get_data(self, url):
         try:
             response = requests.get(url)
-            content.append(response.json())
+            content = response.json()
 
             response.raise_for_status()
         except HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
+            return 0
         except Exception as err:
             print(f'Other error occurred: {err}')
-    return content
+            return 0
+        return content
 
-def print_data():
+    def print_data(self, content, cur):
+        percentage = round(((content["ask"] / content["bid"]) - 1) * 100, 2)
+        print(f"Różnica między kupnem a sprzedażą {cur} za USD wynosi {percentage}%")
 
-    cur1,cur2,cur3 = "BTC","GNT","DASH"
-    url1 = f"https://bitbay.net/API/Public/{cur1}/ticker.json"
-    url2 = f"https://bitbay.net/API/Public/{cur2}/ticker.json"
-    url3 = f"https://bitbay.net/API/Public/{cur3}/ticker.json"
-    while True:
-        content =  get_data(url1,url2,url3)
-        percentage_1 = round(((content[0]["ask"]/content[0]["bid"])-1)*100,2)
-        percentage_2 = round(((content[1]["ask"] / content[1]["bid"]) - 1) * 100, 2)
-        percentage_3 = round(((content[2]["ask"] / content[2]["bid"]) - 1) * 100, 2)
-        print(f"Różnica między kupnem a sprzedażą {cur1} za USD wynosi {percentage_1}%")
-        print(f"Różnica między kupnem a sprzedażą {cur2} za USD wynosi {percentage_2}%")
-        print(f"Różnica między kupnem a sprzedażą {cur1} za USD wynosi {percentage_3}%")
-        time.sleep(5)
+    def get_full_url(self, cur):
+        return self.pre_url + cur + self.post_url
 
-print_data()
+
+    def main(self):
+        while True:
+            for currency in self.currencies:
+                url = self.get_full_url(currency)
+                content = self.get_data(url)
+                self.print_data(content, currency)
+            time.sleep(5)
+
+curr = Finance()
+curr.main()
