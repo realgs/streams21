f403ea295253a64044d551f62c77b3a68e40cf9e
Paul <pawelpelar@gmail.com>
2021-04-07 17:39:19 +0200

    Subplots

--- L2/animate_graph.py
+++ L2/animate_graph.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-
 def animate(i):
     data = pd.read_csv('data.csv')
     x = data['x_val']
@@ -13,19 +12,42 @@ def animate(i):
     bid_cur3 = data['bid_cur3']
     ask_cur3 = data['ask_cur3']
 
-    plt.cla()
+    ax1.cla()
+    ax2.cla()
+    ax3.cla()
+
+    plt.style.use('seaborn')
     currencies = ['BTC', 'GNT', 'DASH']
-    plt.plot(x, bid_cur1, label=f'Bid {currencies[0]}')
-    plt.plot(x, ask_cur1, label=f'Ask {currencies[0]}')
-    plt.plot(x, bid_cur2, label=f'Bid {currencies[1]}')
-    plt.plot(x, ask_cur2, label=f'Ask {currencies[1]}')
-    plt.plot(x, bid_cur3, label=f'Bid {currencies[2]}')
-    plt.plot(x, ask_cur3, label=f'Ask {currencies[2]}')
+    ax1.plot(x, bid_cur1, label=f'Bid')
+    ax1.plot(x, ask_cur1, label=f'Ask')
+    ax2.plot(x, bid_cur2, label=f'Bid')
+    ax2.plot(x, ask_cur2, label=f'Ask')
+    ax3.plot(x, bid_cur3, label=f'Bid')
+    ax3.plot(x, ask_cur3, label=f'Ask')
 
 
-    plt.legend(loc='upper left')
+    ax1.set_title(f'{currencies[0]} and USD')
+    ax1.legend(loc="upper left")
+    ax1.set_xticks([])
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Price')
+
+    ax2.set_title(f'{currencies[1]} and USD')
+    ax2.legend(loc="upper left")
+    ax2.set_xticks([])
+    ax2.set_xlabel('Time')
+    ax2.set_ylabel('Price')
+
+    ax3.set_title(f'{currencies[2]} and USD')
+    ax3.legend(loc="upper left")
+    ax3.set_xticks([])
+    ax3.set_xlabel('Time')
+    ax3.set_ylabel('Price')
+
     plt.tight_layout()
 
-plt.style.use('fivethirtyeight')
+
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=3,ncols=1)
 anim = FuncAnimation(plt.gcf(), animate, interval=1000)
 plt.show()
+
