09de1adbeb5446c1e536fb7b69168f6292897064
Igor Boćkowski <ibockowsky@gmail.com>
2021-04-11 11:47:26 +0200

    chore: add imports

--- app/src/main.js
+++ app/src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/styles/sxcss/index.scss'
+import './assets/styles/sxcss/reset.css'
+import './assets/styles/main.scss'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
