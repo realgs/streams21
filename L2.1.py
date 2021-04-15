99c8ac42b30d944aa1d176d2be119019708de8dd
Bartosz-Stec <71563907+Bartosz-Stec@users.noreply.github.com>
2021-04-15 12:15:09 +0200

    Asks and bids for crypto arguments

--- L2.1.py
+++ L2.1.py
@@ -21,6 +21,18 @@ def crypto_get(data):
         return originalData["bid"], originalData["ask"]
 
 def animate(i):
+    bid_LSK, ask_LSK = crypto_get(f'LSK', 'USD', 'ticker.json')
+    bid_BAT, ask_BAT = crypto_get(f'BAT', 'USD', 'ticker.json')
+    bid_ZRX, ask_ZRX = crypto_get(f'ZRX', 'USD', 'ticker.json')
+
+    x_axis.append(datetime.now().strftime("%H:%M:%S"))
+
+    crypto_bid_LSK.append(ask_LSK)
+    crypto_ask_LSK.append(bid_LSK)
+    crypto_bid_BAT.append(ask_BAT)
+    crypto_ask_BAT.append(bid_BAT)
+    crypto_bid_ZRX.append(ask_ZRX)
+    crypto_ask_ZRX.append(bid_ZRX)
 
 while True:
     now = datetime.now()
