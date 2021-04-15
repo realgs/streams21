9412a152337a857f77e5a1a47dfca91d4ad80cff
Bartosz-Stec <71563907+Bartosz-Stec@users.noreply.github.com>
2021-04-15 12:18:05 +0200

    Plot animation

--- L2.1.py
+++ L2.1.py
@@ -1,7 +1,8 @@
 import requests
 from requests.exceptions import HTTPError
-import time
 from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 URL_START='http://bitbay.net/API/Public/'
@@ -34,12 +35,39 @@ def animate(i):
     crypto_bid_ZRX.append(ask_ZRX)
     crypto_ask_ZRX.append(bid_ZRX)
 
-while True:
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print("Lokalny czas =", dt_string)
-    crypto_get('BTC')
-    crypto_get('DASH')
-    crypto_get('LTC')
-    print('____________________________________________')
-    time.sleep(INTERVAL)
+    x_axis_labels=x_axis.copy()
+
+    # while True:
+    #     i=5
+    #     step=2
+    #     if len(x_axis_labels) > i:
+    #         x_axis_labels = x_axis_labels[::step]
+    #         i+=5
+    #         step+=1
+
+
+    plt.cla()
+    # plt.yscale('log')
+    plt.plot(x_axis, crypto_bid_LSK, linewidth=1, label='Bids_LSK', color='#002e4d')
+    plt.plot(x_axis, crypto_ask_LSK, linewidth=1, label='Asks_LSK', color='#ff99ff')
+    plt.plot(x_axis, crypto_bid_BAT, linewidth=1, label='Bids_BAT', color='#e60000')
+    plt.plot(x_axis, crypto_ask_BAT, linewidth=1, label='Asks_BAT', color='#ff8533')
+    plt.plot(x_axis, crypto_bid_ZRX, linewidth=1, label='Bids_ZRX', color='#00e600')
+    plt.plot(x_axis, crypto_ask_ZRX, linewidth=1, label='Asks_ZRX', color='#bfff00')
+    plt.subplots_adjust(bottom=0.2, left=0.2, right=0.9)
+    plt.xticks(x_axis_labels, rotation=25)
+
+    plt.xlabel('Czas pobrania danych')
+    plt.ylabel('Wartość w USD')
+    plt.legend()
+if __name__ == '__main__':
+    x_axis = []
+    crypto_bid_LSK = []
+    crypto_ask_LSK = []
+    crypto_bid_BAT = []
+    crypto_ask_BAT = []
+    crypto_bid_ZRX = []
+    crypto_ask_ZRX = []
+    ani = FuncAnimation(plt.gcf(),animate , interval=5000)
+    plt.show()
+
