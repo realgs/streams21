bff265d27aee8b00a96d031d082516f3b2c63252
dkosko <dimasikeagle@gmail.com>
2021-04-16 10:04:58 +0300

    change Category

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from math import inf
 import requests
 from matplotlib.animation import FuncAnimation
@@ -6,9 +7,13 @@ import matplotlib.pyplot as plt
 from itertools import count
 
 
-def get_data(crypt, since):
-    url = "https://bitbay.net/API/Public/{Currency[0]}{Currency[1]}/{Category}.json?since={since}".format(Currency=[crypt, 'USD'],
-                                                                                            Category='trades', since=since)
+#liczba punktów na X = 20. jest przemiszczny wykres w prawo
+
+#to co jest w korniu wpisać w __main__
+
+def get_data(crypt, req_type):
+    url = "https://bitbay.net/API/Public/{Currency[0]}{Currency[1]}/{Category}.json".format(Currency=[crypt, 'USD'],
+                                                                                            Category= req_type)
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -18,22 +23,22 @@ def get_data(crypt, since):
 
 
 def count_minmax(data):
-    max_sell = -inf
-    min_buy = inf
+    min_sell = inf
+    max_buy = -inf
     for trade in data:
         if trade['type'] == 'sell':
-            if max_sell < trade['price']:
-                max_sell = trade['price']
+            if min_sell > trade['price']:
+                min_sell = trade['price']
         elif trade['type'] == 'buy':
-            if min_buy > trade['price']:
-                min_buy = trade['price']
+            if max_buy < trade['price']:
+                max_buy = trade['price']
 
-    return max_sell, min_buy
+    return min_sell, max_buy
 
 def data_stream(crypt, x, y_1, y_2, i):
     x.append(i)
-    data = get_data(crypt, since= 50*i)
-    max, min = count_minmax(data)
+    data = get_data(crypt, 'orderbook')
+    min, max = count_minmax(data)
     y_1.append(min)
     y_2.append(max)
     return [x, y_1, y_2]
@@ -41,27 +46,35 @@ def data_stream(crypt, x, y_1, y_2, i):
 
 
 def animate(i):
+    global x, y_1, y_2, stop_point
     if i == stop_point:
         exit('FINISHED')
-    global x, y_1, y_2
     temp = data_stream('DASH', x, y_1, y_2, i)
+    #fig, ax = plt.subplots(1)
     x = temp[0]
     y_1 = temp[1]
     y_2 = temp[2]
-    plt.plot(x, y_1)
-    plt.plot(x, y_2)
-
+    plt.plot(x, y_1, label= 'min1', color= 'red')
+    plt.plot(x, y_2, label= 'max1', color = 'green')
+    plt.legend()
 
 def plot_data():
-    ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+    ani = FuncAnimation(plt.gcf(), func=animate, interval=1000)
 
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Jakiś wykres')
+    #plt.legend()
     plt.tight_layout()
     plt.show()
+
 
 x = []
 y_1 = []
 y_2 = []
 stop_point = 10
 
-plot_data()
-
+#plot_data()
+crypt = 'BTC'
+url = "https://bitbay.net/API/Public/{Currency[0]}{Currency[1]}/{Category}.json".format(Currency=[crypt, 'USD'], Category='market')
+print(url)
