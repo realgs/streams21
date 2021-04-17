2a1cbee09491ee4187cf96ee4fc13a541f95d00c
Mateusz_Mandziuk <254303@student.pwr.edu.pl>
2021-04-17 16:04:04 +0200

    Whole implementation of refreshing plot

--- plotdatastream.py
+++ plotdatastream.py
@@ -1,9 +1,11 @@
 import requests
 import time
-
-oscillation = 5
+import matplotlib.pyplot as plt
+import numpy as np
+interval = 5
 cryptos = ["BTC","ETH","TRX"]
 currency = "USD"
+
 
 def sellbuy_difference(sells, buys):
     sa,sp,ba,bp = 0,0,0,0
@@ -14,6 +16,7 @@ def sellbuy_difference(sells, buys):
     sellprice = sp/sa
     buyprice= bp/ba
     return (1 - (sellprice - buyprice) / buyprice)
+
 
 def handle_exceptions(req):
     try:
@@ -28,11 +31,8 @@ def handle_exceptions(req):
     except requests.exceptions.RequestException as err:
         print("OOps: Something Else", err)
 
-#def graph_gen():
 
-
-
-def cryptostream_to_plot(crypto_set, currency, oscillation):
+def cryptostream_to_plot(crypto_set, currency, interval):
     all_data = []
     while True:
         c_list = []
@@ -41,9 +41,26 @@ def cryptostream_to_plot(crypto_set, currency, oscillation):
             handle_exceptions(response)
             c_list.append([crypto+"/"+currency,sellbuy_difference(response.json()['asks'], response.json()['bids'])])
         all_data.append(c_list)
-        #graph_gen()
-        time.sleep(oscillation)
+        graph_gen(all_data,interval)
 
-cryptostream_to_plot(cryptos,currency, oscillation)
+def graph_gen(all_data,interval):
+    t = np.arange(0,5*(len(all_data)),5)
+    plt.ion()
+    nr = len(all_data[0])
+    for c in range(nr):
+        y = []
+        for set in all_data:
+            y.append(set[c][1])
+        plt.plot(t,y,"--o",label = all_data[0][c][0])
+    plt.legend()
+    plt.xlabel("time")
+    plt.ylabel("value")
+    plt.xlim([-0.25,t[-1]+1])
+    plt.draw()
+    plt.pause(interval)
+    plt.clf()
+
+
+cryptostream_to_plot(cryptos,currency, interval)
 
 
