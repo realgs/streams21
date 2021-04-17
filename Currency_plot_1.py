4247a35f0b1ef5edfdfbefe628dce55cad4960ec
Piterekg9 <piterekg9@wp.pl>
2021-04-17 13:27:46 +0200

    Plot_in_time_data

--- Currency_plot_1.py
+++ Currency_plot_1.py
@@ -20,3 +20,19 @@ def download_data(currency,curr_category,url_beg,url_end): #c_currencies is a li
         print("Could not download data. Try again later!")
         sys.exit()
 
+def plot_data(x_data_BTC,x_data_LTC,x_data_DASH,plt_BTC,plt_LTC,plt_DASH):
+    x_data_BTC.append(datetime.now().strftime("%H:%M:%S"))
+    ask_BTC, bid_BTC = download_data(Currencies[0], Currency_category, url_beg, url_end)
+    x_data_LTC.append(datetime.now().strftime("%H:%M:%S"))
+    ask_LTC, bid_LTC = download_data(Currencies[1], Currency_category, url_beg, url_end)
+    x_data_DASH.append(datetime.now().strftime("%H:%M:%S"))
+    ask_DASH, bid_DASH = download_data(Currencies[2], Currency_category, url_beg, url_end)
+
+    plt_BTC.append(ask_BTC)
+    plt_BTC.append(bid_BTC)
+    plt_LTC.append(ask_LTC)
+    plt_LTC.append(bid_LTC)
+    plt_DASH.append(ask_DASH)
+    plt_DASH.append(bid_DASH)
+
+    return x_data_BTC,x_data_LTC,x_data_DASH,plt_BTC,plt_LTC,plt_DASH
