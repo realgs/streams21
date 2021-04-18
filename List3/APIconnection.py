9af9dfa73b136a033ba7a4d640c229c948e46989
Maciej Majewski <maciekm1799@gmail.com>
2021-04-18 18:38:25 +0200

    Creating functions

--- List3/APIconnection.py
+++ List3/APIconnection.py
@@ -3,23 +3,35 @@ from requests.exceptions import HTTPError
 import time
 
 
-def get_data(target_currency, base_currency, method):
+def get_data(currency_pair, method):
     try:
-        req = requests.get(f'https://bitbay.net/API/Public/{target_currency}{base_currency}/{method}.json')
+        req = requests.get(f'https://bitbay.net/API/Public/{currency_pair}/{method}.json')
         if req.status_code == 200:
             data = req.json()
         else:
-            print("Wystapil blad podczas pobierania -",target_currency + base_currency)
+            print("Wystapil blad podczas pobierania -",currency_pair)
     except HTTPError:
         print('Error:', HTTPError)
         return None
     return data
 
+def create_pairs(BASE, CURR):
+    pairs = []
+    for i in BASE:
+        for j in CURR:
+            pairs.append[i+j]
+    return pairs
+
+def draw_graphs(currency_pairs):
+    pass
+
+def update():
+    pass
 
 def main():
     CURR = ['BTC','DASH','LTC']
     BASE = ['USD']
-    requestoffers(CURR, BASE)
+    requestoffers(create_pairs(BASE, CURR))
 
 
 if __name__ == '__main__':
