2fdcfdb1d973ca8b93941d432aeb3a8506fbb3a4
Michal Chylak <michauchylak@gmail.com>
2021-04-18 22:47:16 +0200

    new imports and market function

--- lista_3/zad_1.py
+++ lista_3/zad_1.py
@@ -1,12 +1,9 @@
 import requests
 from requests.exceptions import HTTPError
-from itertools import count
-import time
-import random
-import openpyxl
-import pandas as pd
+from datetime import datetime
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.dates import DateFormatter
 
 ADRESS = {
     'LTC-PLN' : 'https://bitbay.net/API/Public/LTCPLN/orderbook.json',
@@ -19,34 +16,24 @@ CODES = [200,201,202,203,204,205,206]
 
 
 def market():
-    counter = 1
-    global df_b
-    global df_a
     bids_table = []
     asks_table = []
-    FILE_PATH = 'market.csv'
-    while True:
-        print(counter, "survey of buy - sell difference:")
-        for key in ADRESS.keys():
-            try:
-                request = requests.get(ADRESS[key])
-                if request.status_code in CODES:
-                    bids = request.json()['bids'][0][0]
-                    asks = request.json()['asks'][0][0]
-                    print(key,'->',calc(bids,asks),"%")
-                    bids_table.append(bids)
-                    asks_table.append(asks)
-                    df_b = pd.DataFrame(bids_table)
-                    df_a = pd.DataFrame(asks_table)
-                    df_b = df_b.T
-                    df_a = df_a.T
-                else:
-                    print('ERROR')
-            except HTTPError:
-                print('ERROR: ',HTTPError)
-        counter += 1
-        print('------------------')
-        time.sleep(1)
+    req_times = []
+    for key in ADRESS.keys():
+        try:
+            request = requests.get(ADRESS[key])
+            if request.status_code in CODES:
+                bids = request.json()['bids'][0][0]
+                asks = request.json()['asks'][0][0]
+                req_time = datetime.now()
+                bids_table.append(bids)
+                asks_table.append(asks)
+                req_times.append(req_time)
+            else:
+                print('ERROR')
+        except HTTPError:
+            print('ERROR: ',HTTPError)
+    return bids_table, asks_table, req_times
 
 
 def calc(bids, asks):
@@ -54,5 +41,3 @@ def calc(bids, asks):
     
 
 ani = FuncAnimation(plt.gcf(), market, interval=250)
-
-print(market())
