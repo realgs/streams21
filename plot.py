30f77dcddfe0f8b3bdf5673b2d19c6a4b2a9e885
swojciechowski7 <slawek.woj2000@gmail.com>
2021-04-18 14:42:04 +0200

    Animated plot

--- plot.py
+++ plot.py
@@ -1,5 +1,7 @@
 import requests
 import sys
+import matplotlib.pyplot as plt
+import datetime
 
 
 def path(currency):
@@ -17,3 +19,30 @@ def connect_and_check(currency):
         print("Failed to connect, try again")
         sys.exit()
 
+
+def animated_plot(i):
+    time = datetime.datetime.now()
+    x_val.append(time.strftime('%H:%M:%S'))
+
+    bid_and_ask_BTC = connect_and_check(f'{crypto[0]}{curr}')
+    bid_y_val_BTC.append(bid_and_ask_BTC[0])
+    ask_y_val_BTC.append(bid_and_ask_BTC[1])
+
+    bid_and_ask_LTC = connect_and_check(f'{crypto[1]}{curr}')
+    bid_y_val_LTC.append(bid_and_ask_LTC[0])
+    ask_y_val_LTC.append(bid_and_ask_LTC[1])
+
+    bid_and_ask_DASH = connect_and_check(f'{crypto[2]}{curr}')
+    bid_y_val_DASH.append(bid_and_ask_DASH[0])
+    ask_y_val_DASH.append(bid_and_ask_DASH[1])
+
+    plt.cla()
+    plt.plot(x_val, bid_y_val_BTC)
+    plt.plot(x_val, ask_y_val_BTC)
+    plt.plot(x_val, bid_y_val_LTC)
+    plt.plot(x_val, ask_y_val_LTC)
+    plt.plot(x_val, bid_y_val_DASH)
+    plt.plot(x_val, ask_y_val_DASH)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.legend(["Bid BTC", "Ask BTC", "Bid LTC", "Ask LTC", "Bid DASH", "Ask DASH"])
