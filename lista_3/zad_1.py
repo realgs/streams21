f91decb1b5ba672ab0872f1f23b36bf10f811fac
Michal Chylak <michauchylak@gmail.com>
2021-04-18 23:16:18 +0200

    final version

--- lista_3/zad_1.py
+++ lista_3/zad_1.py
@@ -14,6 +14,13 @@ ADRESS = {
 
 CODES = [200,201,202,203,204,205,206]
 
+INTERVAL = 3000
+
+counter = 0
+currency = []
+for key in ADRESS.keys():
+    currency.append(key)
+
 
 def market():
     bids_table = []
@@ -36,8 +43,55 @@ def market():
     return bids_table, asks_table, req_times
 
 
-def calc(bids, asks):
-    return round(((1 - (asks - bids)/bids)*100),2)
-    
+def arrays(n):
+    return [[] for _ in range(n)]
 
-ani = FuncAnimation(plt.gcf(), market, interval=250)
+
+def show_plots():
+    def update(_):
+        bids, asks, times = market()
+        global counter
+        counter += 1
+        for i in range(n):
+            bids_table[i].append(bids[i])
+            asks_table[i].append(asks[i])
+            req_times[i].append(times[i])
+
+            lines[i*2].set_data(req_times[i], bids_table[i])
+            lines[i*2+1].set_data(req_times[i], asks_table[i])
+            
+            if counter > 20:
+                bids_table[i].pop(0)
+                asks_table[i].pop(0)
+                req_times[i].pop(0)
+                
+
+            axis[i].relim()
+            axis[i].autoscale_view()
+            
+        return lines
+
+    n = 4
+    counter = 0
+    req_times, bids_table, asks_table = arrays(n), arrays(n), arrays(n)
+
+    fig, axis = plt.subplots(n)
+    lines = []
+
+    for i in range(n):
+        bids_part, = axis[i].plot([], [],'g-d', label='Best bid')
+        asks_part, = axis[i].plot([], [],'r-d', label='Best ask')
+        lines.append(bids_part)
+        lines.append(asks_part)
+        
+        axis[i].set_title(currency[i])
+        axis[i].grid()
+        axis[i].legend(loc=6)
+        axis[i].xaxis.set_major_formatter(DateFormatter('%d-%m-%y %H:%M:%S'))
+
+    fig.autofmt_xdate()
+    anim = FuncAnimation(fig, update, interval=INTERVAL)
+    plt.show()
+
+
+show_plots()
