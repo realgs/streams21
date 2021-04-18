d83b78d53189987516666ffccabdcd80afb3909a
Amadeusz <2542569@student.pwr.edu.pl>
2021-04-18 10:34:32 +0200

    add func for live chart without clear()

--- zad2.py
+++ zad2.py
@@ -42,3 +42,31 @@ def plotData(prices):
         prices[cur]['sell'].append(sell)
 
     return prices
+
+def plotLoop():
+    prices_p = plotData(prices)
+    time_data.append(datetime.now().strftime("%H:%M:%S"))
+    
+    for cur in CURRIENCES:
+        ax.plot(time_data[-5:], prices[cur]['buy'][-5:], label=f'{cur} buy')
+        ax.plot(time_data[-5:], prices[cur]['sell'][-5:], label=f'{cur} sell')
+    
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+if __name__  == '__main__':
+    prices = dataFrame()
+    fig = plt.figure()
+    ax =  fig.add_subplot(111)
+    fig.show()
+    time_data = []
+    labels = False
+
+    while True:
+        plotLoop()
+
+        if not labels:
+            ax.legend()
+            labels = True
+
+        sleep(SLEEP_TIME)
