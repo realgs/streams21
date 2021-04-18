c205b63a0331e48bd9f8cb9c8378b395a6f4e0d7
Ola <olek@rafkos.net>
2021-04-19 01:22:32 +0200

    time fixed

--- l3.py
+++ l3.py
@@ -9,12 +9,12 @@ def get_data(currency, retry_stat_code_err, retry_connect_err):
     url_post = '/ticker.json'
     url = url_pre + currency + url_post
     try:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
         response = requests.get(url)
         status_code = str(response.status_code)
         if status_code[0] == '2':
             resp_json = response.json()
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
             selling_cost = resp_json['ask']
             purchase_cost = resp_json['bid']
             return current_time, selling_cost, purchase_cost
@@ -54,16 +54,16 @@ def draw_plot(currency_list, time_interval):
         update_data(values)
         for n in range(cur_num):
             if len(values[currency_list[n]]) == 2:
-                time = [values[currency_list[n]][-1][0], values[currency_list[n]][-2][0]]
-                sell = [values[currency_list[n]][-1][1], values[currency_list[n]][-2][1]]
-                purchase = [values[currency_list[n]][-1][2], values[currency_list[n]][-2][2]]
+                time = [values[currency_list[n]][-2][0], values[currency_list[n]][-1][0]]
+                sell = [values[currency_list[n]][-2][1], values[currency_list[n]][-1][1]]
+                purchase = [values[currency_list[n]][-2][2], values[currency_list[n]][-1][2]]
                 axs[n].plot(time, sell, label="Selling cost", color='lime')
                 axs[n].plot(time, purchase, label="Purchase cost", color='m')
                 axs[n].legend()
             elif len(values[currency_list[n]]) > 2:
-                time = [values[currency_list[n]][-1][0], values[currency_list[n]][-2][0]]
-                sell = [values[currency_list[n]][-1][1], values[currency_list[n]][-2][1]]
-                purchase = [values[currency_list[n]][-1][2], values[currency_list[n]][-2][2]]
+                time = [values[currency_list[n]][-2][0], values[currency_list[n]][-1][0]]
+                sell = [values[currency_list[n]][-2][1], values[currency_list[n]][-1][1]]
+                purchase = [values[currency_list[n]][-2][2], values[currency_list[n]][-1][2]]
                 axs[n].plot(time, sell, color='lime')
                 axs[n].plot(time, purchase, color='m')
         plt.pause(time_interval)
