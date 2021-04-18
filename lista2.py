9f23fa2667f6a0a061f4878be16b15f9eaceda8f
Iga-Miller <80620641+Iga-Miller@users.noreply.github.com>
2021-04-18 12:12:08 +0200

    Lista2

--- lista2.py
+++ lista2.py
@@ -2,37 +2,35 @@ import requests
 import time
 from requests.exceptions import HTTPError
 
-url={'BTCUSD':'https://bitbay.net/API/Public/BTCUSD/orderbook.json',
-     'LTCUSD':'https://bitbay.net/API/Public/LTCUSD/orderbook.json',
-     'DASHUSD':'https://bitbay.net/API/Public/DASHUSD/orderbook.json'}
+url_1 = 'https://bitbay.net/API/Public/'
+url_2 = '/orderbook.json'
+base = 'USD'
+currencies = ['BTC', 'LTC', 'DASH']
+frequency = 5
+
+def data(currency):
+    try:
+        response = requests.get(url_1 + currency + base + url_2)
+        response.raise_for_status()
+        return response.json()
+    except HTTPError:
+        print('HTTP error:', HTTPError)
+    except Exception:
+        print('Other error:', Exception)
 
 def offers():
-     for u in url.keys():
-          try:
-               response=requests.get(url[u])
-               print(u,response.json())
-               response.raise_for_status()
-          except HTTPError:
-               print('HTTP error:', HTTPError)
-          except Exception:
-               print('Other error:',Exception)
+    for currency in currencies:
+        response = data(currency)
+        print(currency, response)
 
-
-def bids_asks_diff():
-     while True:
-          for u in url.keys():
-               try:
-                    response=requests.get(url[u])
-                    buy = response.json()['bids'][0][0]
-                    sell = response.json()['asks'][0][0]
-                    difference = 1-(sell-buy)/sell
-                    print(u, difference)
-               except HTTPError:
-                    print('HTTP error:', HTTPError)
-               except Exception:
-                    print('Other error:', Exception)
-          time.sleep(5)
+    while True:
+        for currency in currencies:
+            response = data(currency)
+            buy = response['bids'][0][0]
+            sell = response['asks'][0][0]
+            difference = 1 - (sell - buy) / sell
+            print(currency, difference)
+        time.sleep(frequency)
 
 
 offers()
-bids_asks_diff()
