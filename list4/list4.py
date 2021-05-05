ec922707ed3f2dc784e8f17b068b10fbec033784
AKulakowska <254311@student.pwr.edu.pl>
2021-05-05 20:27:57 +0200

    fixed downloading volume

--- list4/list4.py
+++ list4/list4.py
@@ -5,8 +5,8 @@ from matplotlib.animation import FuncAnimation
 
 CURRENCY = ["BTC-PLN", "BCC-PLN", "ETH-PLN"]
 N = int(input('Podaj z ilu ostatnich probek liczyć średnią (max 20):'))
-START = int(input('Podaj poczatek przedziału y do wiliczenia rsi:'))
-STOP = int(input('Podaj koniec przedziału y do wiliczenia rsi:'))
+START = int(input('Podaj poczatek przedziału y do wiliczenia rsi (zakres 0,20):'))
+STOP = int(input('Podaj koniec przedziału y do wiliczenia rsi (zakres 0,20):'))
 
 buys = []
 sells = []
@@ -24,18 +24,25 @@ def get_data(currency):
     response = eval(response.text)
     buy = response['ask']
     sell = response['bid']
-    volume = response['volume']
-    return buy, sell, volume
+    return buy, sell
+
+
+def get_volumen(currency):
+    url = f"https://api.bitbay.net/rest/trading/transactions/{currency}"
+    response = requests.get(url)
+    response = eval(response.text)
+    return float(response['items'][1]['a'])
 
 
 def average(data_list, n):
-    part_data = data_list[len(data_list)-n:]
+    part_data = data_list[-n:]
     avg = sum(part_data)/len(part_data)
     return avg
 
 
 def count_rsi(data_list, start, stop):
-    part_data = data_list[start:stop]
+    part_data = data_list[-20:]
+    part_data = part_data[start:stop]
     rises = 0
     rises_counter = 0
     losses = 0
@@ -62,28 +69,28 @@ def count_rsi(data_list, start, stop):
 
 
 def add_data(currency, buy_list, sell_list, avg_buy_list, avg_sell_list, volumen_list, rsi_buy_list, rsi_sell_list):
-    buy, sell, volumen = get_data(currency)
+    buy, sell = get_data(currency)
     buy_list.append(buy)
     sell_list.append(sell)
     buy_avg = average(buy_list, N)
     sell_avg = average(sell_list, N)
     avg_buy_list.append(buy_avg)
     avg_sell_list.append(sell_avg)
-    volumen_list.append(volumen)
+    volumen_list.append(get_volumen(currency))
     rsi_buy_list.append(count_rsi(buy_list, START, STOP))
     rsi_sell_list.append(count_rsi(sell_list, START, STOP))
     return buy_list, sell_list, avg_buy_list, avg_sell_list, volumen_list, rsi_buy_list, rsi_sell_list
 
 
 def make_plot(a):
-    y1, y2, avg1, avg2, v, rsi1, rsi2 = add_data(CURRENCY[0], buys, sells, avg_buy, avg_sell, volume, rsi_buy_values, rsi_sell_values)
+    y1, y2, avg1, avg2, v, rsi1, rsi2 = add_data(CURRENCY[2], buys, sells, avg_buy, avg_sell, volume, rsi_buy_values, rsi_sell_values)
     t.append(time.strftime("%H:%M:%S", time.localtime()))
     for i in [y1, y2, avg1, avg2, v, rsi1, rsi2, t]:
         if len(i) > 20:
             i.pop(0)
     plt.clf()
     plt.subplot(311)
-    plt.title(f'{CURRENCY[0]} chart')
+    plt.title(f'{CURRENCY[2]} chart')
     plt.plot(t, y1, label="buys", color="blue")
     plt.plot(t, y2, label="sells", color="yellow")
     plt.plot(t, avg1, '--', label='buy avg', color='green')
