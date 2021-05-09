c0d858961a64d9c4249168c4e461d735b1a50228
Piotr (pkk0) Uliński <pkk0@users.noreply.github.com>
2021-05-09 15:07:15 +0200

    Implement fetching best offers

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from datetime import datetime
+
+BITBAY_API_URL = 'https://api.bitbay.net/rest/trading/orderbook-limited/CURRENCY_PAIR/10'
+
+
+def fetch_best_offers(currency_pair):
+    try:
+        response = requests.get(BITBAY_API_URL.replace('CURRENCY_PAIR', currency_pair)).json()
+
+        return {
+            'sell': float(response['sell'][0]['ra']),
+            'buy': float(response['buy'][0]['ra']),
+            'volume': float(response['sell'][0]['ca']) + float(response['buy'][0]['ca']),
+            'time': datetime.now()
+        }
+    except Exception as e:
+        print('fetch_best_offers(): Something went wrong during fetching best offers from orderbook.')
+        print(f'Error: {e}\n')
+        return False
+
