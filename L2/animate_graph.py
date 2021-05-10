d9c10eb4774e1ec6bcbef9a53b53fd9ab3a95dc0
Paul <pawelpelar@gmail.com>
2021-05-10 10:28:13 +0200

    average added

--- L2/animate_graph.py
+++ L2/animate_graph.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import numpy as np
 
 
 def every_nth_func(x_val):
@@ -39,10 +40,28 @@ def count_volumen():
         sum_3 += elem[1]
     return [sum_1, sum_2, sum_3]
 
+def average(mylist):
+    mylist = list(mylist)
+    N = 3
+    cumsum, moving_aves = [0], []
+    moving_aves.append(mylist[0])
+    for i, x in enumerate(mylist, 1):
+        cumsum.append(cumsum[i - 1] + x)
+        if i >= N:
+            moving_ave = (cumsum[i] - cumsum[i - N]) / N
+            moving_aves.append(moving_ave)
+    moving_aves.append(mylist[-1])
+    return moving_aves
+
+def total_avg(mylist):
+    mylist = list(mylist)
+    return sum(mylist)/len(mylist)
+
 
 def animate(i):
     data = pd.read_csv('data.csv')
     x = data['time']
+    len_data = len(data)
     bid_cur1 = data['bid_cur1']
     ask_cur1 = data['ask_cur1']
     bid_cur2 = data['bid_cur2']
@@ -67,6 +86,32 @@ def animate(i):
     ax3.plot(x, ask_cur2, label=f'Ask')
     ax5.plot(x, bid_cur3, label=f'Bid')
     ax5.plot(x, ask_cur3, label=f'Ask')
+
+    sliced_data = data[int(len_data/3):]
+    sliced_x = sliced_data['time']
+    sliced_bid_cur1 = sliced_data['bid_cur1']
+    sliced_ask_cur1 = sliced_data['ask_cur1']
+    average_bid_cur1 = average(sliced_bid_cur1)
+    average_ask_cur1 = average(sliced_ask_cur1)
+
+    ax1.plot(sliced_x, average_bid_cur1, label=f'AVG bid, TOTAL: {round(total_avg(sliced_bid_cur1))}')
+    ax1.plot(sliced_x, average_ask_cur1, label=f'AVG ask, TOTAL: {round(total_avg(sliced_ask_cur1))}')
+
+    sliced_bid_cur2= sliced_data['bid_cur2']
+    sliced_ask_cur2 = sliced_data['ask_cur2']
+    average_bid_cur2 = average(sliced_bid_cur2)
+    average_ask_cur2 = average(sliced_ask_cur2)
+
+    ax3.plot(sliced_x, average_bid_cur2, label=f'AVG bid, TOTAL: {round(total_avg(sliced_bid_cur2))}')
+    ax3.plot(sliced_x, average_ask_cur2, label=f'AVG ask, TOTAL: {round(total_avg(sliced_ask_cur2))}')
+    
+    sliced_bid_cur3= sliced_data['bid_cur3']
+    sliced_ask_cur3 = sliced_data['ask_cur3']
+    average_bid_cur3 = average(sliced_bid_cur3)
+    average_ask_cur3 = average(sliced_ask_cur3)
+
+    ax5.plot(sliced_x, average_bid_cur3, label=f'AVG bid, TOTAL: {round(total_avg(sliced_bid_cur3))}')
+    ax5.plot(sliced_x, average_ask_cur3, label=f'AVG ask, TOTAL: {round(total_avg(sliced_ask_cur3))}')
 
 
     ax2.barh('-', volume[0], align='edge')
