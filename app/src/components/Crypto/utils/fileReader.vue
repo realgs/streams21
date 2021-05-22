5a8f0e869d01abd2498c686eca68acde25833b4d
Igor Boćkowski <ibockowsky@gmail.com>
2021-05-22 15:50:58 +0200

    feat: implement file reader

---
+++ app/src/components/Crypto/utils/fileReader.vue
@@ -0,0 +1,29 @@
+<template>
+  <label class="text-reader">
+    <input type="file" @change="loadTextFromFile" />
+  </label>
+</template>
+
+<script>
+export default {
+  methods: {
+    loadTextFromFile(ev) {
+      const file = ev.target.files[0]
+      const reader = new FileReader()
+
+      reader.onload = (e) => this.$emit('load', e.target.result)
+      reader.readAsText(file)
+    },
+  },
+}
+</script>
+
+<style>
+input {
+  background: #555;
+  color: white;
+  border: 1px solid #333;
+  border-radius: 10%;
+  padding: 4px 16px;
+}
+</style>
