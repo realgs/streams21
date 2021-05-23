50d4ec406fada51ae98a839e33e85827f304e914
sniegbanan <michalchoczaj@icloud.com>
2021-05-23 21:57:02 +0200

    RSI wskaznik, kandydat, liquid i spread

--- l5.py
+++ l5.py
@@ -87,8 +87,67 @@ def trend(rsi_list):
         return 'upward trend'
     elif rsi <= 30:
         return 'downward trend'
-    else:
-        return 'sideways trend'
+    return 'sideways trend'
+
+def volumelist(btc,eth,omg):
+    volume_list = [btc[-1],eth[-1],omg[-1]]
+    return volume_list
+
+def trendlist(btc,eth,omg):
+    trend_list = []
+    for rsi in [btc,eth,omg]:
+        trend_list.append(trend(rsi))
+    return trend_list
+
+def candidate():
+    volume_list = volumelist(btc=BTCvol,eth=ETHvol,omg=OMGvol)
+    trend_list = trendlist(btc=rsi_askBTC,eth=rsi_askETH,omg=rsi_askOMG)
+    choosing_dict = {}
+
+    for value in range (3):
+        if trend_list[value] != "downward trend":   
+            choosing_dict[volume_list[value]] = value
+    if len(choosing_dict) != 0:
+        candidate = max(choosing_dict)
+        return CRYPTOS[choosing_dict[candidate]]
+    print(f"Kandydat nie istnieje")
+    return False
+
+def candidateUtil():
+    candidateUtil = candidate()
+    if candidateUtil:
+        for index,crypto in enumerate(CRYPTOS):
+            if crypto == candidateUtil:
+                return index
+    return False  
+
+def volatileUtil(buy_list, X=10, Y=5):
+    Y_list = buy_list[-Y:]
+    value = (abs(max(Y_list) - min(Y_list)) / max(Y_list)) * 100
+    if value > X:
+        return 1
+    return 0
+
+def liquidUtil(buy_list, sell_list, S=4):
+    bid = sell_list[-1]
+    ask = buy_list[-1]
+    spread = ((ask - bid) / ask ) * 100
+    if spread < S:
+        return 1
+    return 0
+
+def titleUtil(bid,ask):
+    volt = volatileUtil(bid)
+    liquid = liquidUtil(bid,ask)
+
+    if volt == 0 and liquid == 0:
+        return "No assets"
+    elif volt == 1 and liquid == 0:
+        return "Volatile asset"
+    elif volt == 1 and liquid == 1:
+        return "Volatile and liquid assets"
+    elif volt == 0 and liquid == 1:
+        return "Liquid asset" 
 
 def dataslist(currency):
     bid, ask = datas(f"{CRYPTOS[0]}{currency}")
@@ -121,12 +180,18 @@ def plot(i):
     x.append(time.strftime('%H:%M:%S'))
 
     dataslist("PLN")
+    trendlist(rsi_askBTC,rsi_askETH,rsi_askOMG)
+    candidate = candidateUtil()
+
 
     ax[0][0].cla()
     ax[0][0].plot(x,BTCbid,'b', label = "BTCbid")
     ax[0][0].plot(x,BTCask,'r',label = "BTCask")
     ax[0][0].set_xlim(auto = 1)
     ax[0][0].set_title(f"{CRYPTOS[0]} values")
+    if candidate == 0:
+        ax[0][0].set_title(f"Kandydat",loc="left")
+        ax[0][0].set_title(f"{titleUtil(BTCbid,BTCask)}",loc="right")
     ax[0][0].set_ylabel('Value [PLN]')
     ax[0][0].set_xlabel('Time')
     ax[0][0].text(0.3, 0.2, f"Volume: {round(BTCvol[-1],3)}", horizontalalignment='center',
@@ -153,6 +218,9 @@ def plot(i):
     ax[1][0].plot(x,ETHask,'r',label = "ETHask")
     ax[1][0].set_xlim(auto = 1)
     ax[1][0].set_title(f"{CRYPTOS[1]} values")
+    if candidate == 1:
+        ax[1][0].set_title(f"Kandydat",loc="left")
+        ax[1][0].set_title(f"{titleUtil(ETHbid,ETHask)}",loc="right")
     ax[1][0].set_ylabel('Value [PLN]')
     ax[1][0].set_xlabel('Time')
     ax[1][0].text(0.3, 0.2, f"Volume: {round(ETHvol[-1],3)}", horizontalalignment='center',
@@ -179,6 +247,9 @@ def plot(i):
     ax[2][0].plot(x,OMGask,'r',label = "OMGask")
     ax[2][0].set_xlim(auto = 1)
     ax[2][0].set_title(f"{CRYPTOS[2]} values")
+    if candidate == 2:
+        ax[2][0].set_title(f"Kandydat",loc="left")
+        ax[2][0].set_title(f"{titleUtil(OMGbid,OMGask)}",loc="right")
     ax[2][0].set_ylabel('Value [PLN]')
     ax[2][0].set_xlabel('Time')
     ax[2][0].text(0.3, 0.2, f"Volume: {round(OMGvol[-1],3)}", horizontalalignment='center',
@@ -206,7 +277,7 @@ def plot(i):
 
 
 
-CRYPTOS = ["BTC","ETH","OMG"]
+CRYPTOS = ["DASH","LTC","LSK"]
 CURRENCY = "PLN"
 
 
