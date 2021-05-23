4816b7e8cf9cf264eedaaf87f16fdd79d6c42581
dariakowal <72565933+dariakowal@users.noreply.github.com>
2021-05-23 19:00:52 +0200

    fin point 3

--- L5.py
+++ L5.py
@@ -1,23 +1,19 @@
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 import requests
 import time
 
 base_currency = 'PLN'
-url = 'https://bitbay.net/API/Public/'
-post = '/trades.json'
-time_interval = 3
-
-# w_volume = int(input('Okno przesuwne dla wolumenu: '))
-# w_mean = int(input('Okno przesuwne dla średniej: '))
-# w_rsi = int(input('Okno przesuwne dla RSI: '))
+url1 = 'https://bitbay.net/API/Public/'
+post1 = '/trades.json'
+post2 = '/orderbook.json'
+time_interval = 4
 
 w_volume = 2
 w_mean = 2
 w_rsi = 2
 
 
-def get_data(currency_list, pos):
+def get_data(url, currency_list, pos):
     try:
         req = requests.get(url + currency_list + base_currency + pos)
         req.raise_for_status()
@@ -27,7 +23,7 @@ def get_data(currency_list, pos):
         raise SystemExit(err)
 
 
-def calculate_rsi(data, w):  # dodać obsługę dziekenia przez 0!!
+def calculate_rsi(data, w):
     data = data[-w:]
     rise = 0
     r_count = 0
@@ -48,6 +44,8 @@ def calculate_rsi(data, w):  # dodać obsługę dziekenia przez 0!!
         b = 1
     else:
         b = loss / l_count
+        if b == 0:
+            b = 1
     rsi_value = 100 - (100 / (1 + (a / b)))
 
     return rsi_value
@@ -89,6 +87,9 @@ def create_plot(currency_list):
 
 if __name__ == '__main__':
     currencies = ['DASH', 'OMG', 'BTC']
+    Y = 3
+    X = 0.5
+    S = 0.1
     n = len(currencies)
 
     price = {}
@@ -96,12 +97,15 @@ if __name__ == '__main__':
     amount = {}
     mean = {}
     rsi = {}
+    difference = {}
+
     for currency in currencies:
         price[currency] = []
         volume[currency] = []
         amount[currency] = []
         mean[currency] = []
         rsi[currency] = []
+        difference[currency] = []
 
     times = []
     t = 0
@@ -114,9 +118,13 @@ if __name__ == '__main__':
             t += 1
             times.append(t)
             for currency in currencies:
-                r = get_data(currency, post)
+                r = get_data(url1, currency, post1)
                 price[currency].append(r[i]['price'])
                 amount[currency].append(r[i]['amount'])
+                r2 = get_data(url1, currency, post2)
+
+                result = (r2['asks'][0][0] - r2['bids'][0][0]) / r2['bids'][0][0]
+                difference[currency].append(round(result, 2))
 
                 if len(price[currency]) >= w_volume:
                     volume[currency].append(calculate_volume(amount[currency], w_volume))
@@ -145,17 +153,27 @@ if __name__ == '__main__':
                             rsi[currencies[j]]) * 2.1
                 ax[j].set_ylim(minim, maxim)
                 ax[j].set_xlim(0, max(times) + 15)
-                # ax[j].set_yscale('symlog')
+                ax[j].set_yscale('symlog')
 
-                ax[j].text(1.3, .85, '          ', fontsize=10, transform=ax[j].transAxes,
-                           bbox=dict(facecolor='white', edgecolor="none", alpha=1))
-                ax[j].text(1.03, .85, 'typ trendu: ', fontsize=10, transform=ax[j].transAxes,
+                ax[j].text(1.03, .85, '                                       ', fontsize=10, transform=ax[j].transAxes,
                            bbox=dict(facecolor='white', edgecolor="none", alpha=1))
 
-                ax[j].text(1.03, .65, '                ', fontsize=10, transform=ax[j].transAxes,
+                ax[j].text(1.03, .65, '                                       ', fontsize=10, transform=ax[j].transAxes,
                            bbox=dict(facecolor='white', edgecolor="none", alpha=1))
+
+                ax[j].text(1.03, .45, '                                       ', fontsize=10, transform=ax[j].transAxes,
+                           bbox=dict(facecolor='white', edgecolor="none", alpha=1))
+
+                ax[j].text(1.03, .25, '                                       ', fontsize=10, transform=ax[j].transAxes,
+                           bbox=dict(facecolor='white', edgecolor="none", alpha=1))
+
+                ax[j].text(1.03, .85, 'typ trendu: ', fontsize=10, transform=ax[j].transAxes)
+
+                ax[j].text(1.03, .65, 'kandydat:', fontsize=10, transform=ax[j].transAxes)
 
                 if len(rsi[currencies[j]]) >= 2:
+                    ax[j].text(1.3, .65, 'nie', c='red', fontsize=10, transform=ax[j].transAxes)
+
                     if rsi[currencies[j]][-1] > 70 or rsi[currencies[j]][-2] > 50 and rsi[currencies[j]][-1] > 50:
                         ax[j].text(1.3, .85, 'spadek', fontsize=10, c='red', transform=ax[j].transAxes)
 
@@ -163,20 +181,42 @@ if __name__ == '__main__':
                         ax[j].text(1.3, .85, 'wzrost', fontsize=10, c='green', transform=ax[j].transAxes)
 
                     else:
-                        ax[j].text(1.3, .85, '------', fontsize=10, transform=ax[j].transAxes)
+                        ax[j].text(1.3, .85, 'boczny', fontsize=10, c='blue', transform=ax[j].transAxes)
 
             temp_volume = []
             for j in range(n):
-                if len(volume[currencies[j]]) >= 2:
+                if len(volume[currencies[j]]) >= 2 and not (
+                        rsi[currencies[j]][-1] > 70 or rsi[currencies[j]][-2] > 50 and rsi[currencies[j]][-1] > 50):
                     temp_volume.append((volume[currencies[j]][-1], j))
 
-            if len(volume[currencies[0]]) >= 2:
+            if len(volume[currencies[0]]) >= 2 and len(temp_volume) >= 1:
                 temp_volume.sort(key=lambda tup: tup[0])
                 max_volume = temp_volume[-1]
                 j = max_volume[1]
 
-                if not (rsi[currencies[j]][-1] > 70 or rsi[currencies[j]][-2] > 50 and rsi[currencies[j]][-1] > 50):
-                    ax[max_volume[1]].text(1.03, .65, 'kandydat', fontsize=10, c='blue', transform=ax[max_volume[1]].transAxes)
+                ax[j].text(1.3, .65, '    ', fontsize=10, transform=ax[j].transAxes,
+                           bbox=dict(facecolor='white', edgecolor="none", alpha=1))
+
+                ax[j].text(1.3, .65, 'tak', fontsize=10, c='green', transform=ax[j].transAxes)
+
+                ax[j].text(1.03, .45, 'volatile asset:', fontsize=10, transform=ax[j].transAxes)
+
+                ax[j].text(1.03, .25, 'liquid asset:', fontsize=10, transform=ax[j].transAxes)
+
+                if len(price[currencies[j]]) >= Y:
+                    minimum = min(price[currencies[j]][-Y:])
+                    maximum = max(price[currencies[j]][-Y:])
+                    volatile = (maximum - minimum) / maximum
+
+                    if volatile >= X:
+                        ax[j].text(1.3, .25, 'tak', c='green', fontsize=10, transform=ax[j].transAxes)
+                    else:
+                        ax[j].text(1.3, .25, 'nie', c='red', fontsize=10, transform=ax[j].transAxes)
+
+                    if difference[currencies[j]][-1] <= S:
+                        ax[j].text(1.3, .45, 'tak', c='green', fontsize=10, transform=ax[j].transAxes)
+                    else:
+                        ax[j].text(1.3, .45, 'nie', c='red', fontsize=10, transform=ax[j].transAxes)
 
             fig.canvas.draw()
             fig.canvas.flush_events()
