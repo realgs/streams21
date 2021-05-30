aa9dcd10c9cbc19d87dfc1c51ea8ca5fa2103332
nielogicznie <254292@student.pwr.edu.pl>
2021-05-30 22:09:14 +0200

    base for l5 on lists

--- l5.py
+++ l5.py
@@ -4,7 +4,7 @@ import numpy as np
 from sys import exit
 from datetime import datetime, timedelta
 
-def link(currency_type, type): #sklejone wszystkie url
+def link(currency_type, type):
     if type == 'ticker':
         return f'https://bitbay.net/API/Public/{currency_type}/ticker.json'
     if type == 'transactions':
@@ -17,7 +17,7 @@ def get_data(currency_type):
         bid = req_json['bid']
         ask = req_json['ask']
         print (req.status_code)
-    except Exception as e: # obsługa błęów błędy obsługi -> status code
+    except Exception as e:
         print(e)
         print (req.status_code)
         return {}
@@ -33,16 +33,21 @@ def get_volumen(currency_type):
     return sum([float(response['items'][i]['a']) for i in range(len(response['items']))])
 
 
-
-def list_average(list_, elements_a):# blad krotkosci - nie ma: jezeli probek jest mniej, to używamy wszytskie które są
-    data = list_[-elements_a:]
-    if elements_a > len (list_) - 3:
-        print ("The number of elements is smaller than list length, but it doesn't matter bcs we use all we have")
-        print(data)
-    return sum(data) / len(data)
+def list_average(list_, elements_a):
+    clean = []
+    for i in range(len(list_)-1, 0, -3):
+        clean.append(list_[i])
+    data = clean[-elements_a:]
+    a = len(data)
+    if len(data) == 0:
+        return None
+    return sum(data) / a
 
 def RSI_value(list_, elements_r):
-    data = list_[- elements_r:]
+    clean = []
+    for i in range(len(list_)-1, 0, -3):
+        clean.append(list_[i])
+    data = clean[- elements_r:]
     up = 0
     up_c = 0
     down = 0
@@ -62,8 +67,8 @@ def RSI_value(list_, elements_r):
         b = 1
     else:
         b = down/down_c
-    RSI = 100 - (100 / (1 + (a / b)))
-    return RSI
+    rsi = 100 - (100 / (1 + (a / b)))
+    return rsi
 
 def draw_axes():
     fig, axs = plt.subplots(3, 3, figsize=(12, 5), constrained_layout=True)
@@ -80,26 +85,6 @@ def draw_axes():
         axs[2][i].set_ylabel('Value')
     return fig, axs
 
-
-def add_avgRSI(buy_list, sell_list, avg_buy_list, avg_sell_list, RSI_buy_list, RSI_sell_list, currency_type):
-    buy, sell = get_data(currency_type)
-    buy_list.append(buy)
-    sell_list.append(sell)
-    buy_avg = list_average(buy_list, elements_a)
-    sell_avg = list_average(sell_list, elements_a)
-    avg_buy_list.append(buy_avg)
-    avg_sell_list.append(sell_avg)
-    RSI_buy_list.append(RSI_value(buy_list, elements_r))
-    RSI_sell_list.append(RSI_value(sell_list, elements_r))
-    return avg_buy_list, avg_sell_list, RSI_buy_list, RSI_sell_list
-
-
-def add_values(buy, sell, volume, currency_type):
-    data = get_data(currency_type)
-    buy.append(data[0])
-    sell.append(data[1])
-    volume.append(get_volumen(currency_type))
-    return buy, sell, volume
 
 def check_trend(list):
     avg = list_average(list, len(list))
@@ -136,22 +121,42 @@ def draw_plot(time_interval):
     i = 0
     while True:
         for a, currency_type in enumerate(CURRENCIES):
-            add_values(buy, sell, volume, currency_type)
-            avg_buy, avg_sell, buy_RSI, sell_RSI = add_avgRSI(buy_list, sell_list, avg_buy_list, avg_sell_list, rsi_buy_list, rsi_sell_list, currency_type)
-            if len(buy) and len(sell) and len(buy_RSI) and len(sell_RSI) >= 2:
+
+            data = get_data(currency_type)
+            buy.append(data[0])
+            sell.append(data[1])
+            volume.append(get_volumen(currency_type))
+            print( volume)
+
+            buy_, sell_ = get_data(currency_type)
+            buy_list.append(buy_)
+            sell_list.append(sell_)
+
+            buy_avg = list_average(buy_list, elements_a)
+            sell_avg = list_average(sell_list, elements_a)
+
+            avg_buy_list.append(buy_avg)
+            avg_sell_list.append(sell_avg)
+            rsi_buy_list.append(RSI_value(buy_list, elements_r))
+            rsi_sell_list.append(RSI_value(sell_list, elements_r))
+
+
+            if len(buy) and len(sell) and len(rsi_buy_list) and len(rsi_sell_list) >= 4:
         
                 x = [(i - 1) * time_interval, i * time_interval]
-                selling_cost = [sell[-2], sell[-1]]
-                purchase_cost = [buy[-2], buy[-1]]
-                volume_value = [volume[-2], volume[-1]]
-                avg_buy_value = [avg_buy[-2], avg_buy[-1]]
-                avg_sell_value = [avg_sell[-2], avg_sell[-1]]
-                RSI_buy_value = [buy_RSI[-2], buy_RSI[-1]]
-                RSI_sell_value = [sell_RSI[-2], sell_RSI[-1]]
+                selling_cost = [sell[-4], sell[-1]]
+                purchase_cost = [buy[-4], buy[-1]]
+                volume_value = [volume[-4], volume[-1]]
+                avg_buy_value = [avg_buy_list[-4], avg_buy_list[-1]]
+                avg_sell_value = [avg_sell_list[-4], avg_sell_list[-1]]
+                RSI_buy_value = [rsi_buy_list[-4], rsi_buy_list[-1]]
+                RSI_sell_value = [rsi_sell_list[-4], rsi_sell_list[-1]]
+
                 axs[0][a].plot(x, selling_cost, label="Selling cost", color='red')
                 axs[0][a].plot(x, purchase_cost, label="Purchase cost", color='yellow')
                 axs[0][a].plot(x, avg_buy_value, '--', label='Average Buy Cost', color='darkred')
                 axs[0][a].plot(x, avg_sell_value, '--', label='Average Sell Cost', color='#9B870C')
+
 
                 axs[1][a].bar(x, volume_value, label='Volume ', color='purple')
                 axs[1][a].set_xticks([])
@@ -159,14 +164,13 @@ def draw_plot(time_interval):
                 axs[2][a].plot(x, RSI_buy_value, label='RSI buy', color='lightgreen' )
                 axs[2][a].plot(x, RSI_sell_value, label='RSI sell', color='darkgreen')
 
-                if len(buy) and len(sell) and len(buy_RSI) and len(sell_RSI) == 2:
+                if len(buy) and len(sell) and len(rsi_buy_list) and len(rsi_sell_list) == 6:
                     axs[0][2].legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
                     axs[1][2].legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
                     axs[2][2].legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
                 
         i += 1
         plt.pause(time_interval)
-
 
 def check_value(name):
     if str(name) == 'c':
