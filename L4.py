2aa2f9b0b14b0b21600b991682f077d5eaaa1612
aleksandraJ11 <jaleksandra16@gmail.com>
2021-06-02 19:36:54 +0200

    real time added

--- L4.py
+++ L4.py
@@ -2,10 +2,17 @@ from requests import get
 import matplotlib.pyplot as plt
 import sys
 import requests
+from datetime import datetime, timedelta
+from matplotlib.ticker import MaxNLocator
 
 
 def connection(curr):
     return f'https://bitbay.net/API/Public/{curr}/ticker.json'
+
+
+def get_time(current_time):
+    current_time.append(datetime.now())
+    return current_time
 
 
 def get_values(curr):
@@ -17,40 +24,34 @@ def get_values(curr):
     except Exception as e:
         print(e)
 
-
 def get_volumen(curr):
-    URL = f"https://api.bitbay.net/rest/trading/transactions/{curr}"
-    response = requests.get(URL)
+    fromtime = int((datetime.now() - timedelta(seconds=60)).timestamp()) * 1000
+    url = f"https://api.bitbay.net/rest/trading/transactions/{curr}"
+    queryparams = {'fromTime': fromtime}
+    response = requests.request("GET", url, params=queryparams)
     response = eval(response.text)
-    volumen = float(response['items'][1]['a'])
-    return volumen
+    return sum([float(response['items'][i]['a']) for i in range(len(response['items']))])
+
+
+def gen_empty_dicts(list_currencies):
+    return {curr: [] for curr in list_currencies}
 
 
 
-def add_values(buy, sell, volume):
-    data = get_values(curr)
-    buy.append(data[0])
-    sell.append(data[1])
-    value_volume = get_volumen(curr)
-    volume.append(value_volume)
-    return buy, sell, volume
-
-
-def calculate_averange(data_list):
-    interval_data = data_list[-qty:]
+def calculate_averange(data_dict, curr):
+    interval_data = data_dict[curr][-qty:]
     avg = sum(interval_data) / len(interval_data)
     return avg
 
 
-def calculate_RSI(data_list):
-    interval_data = data_list[-20:]
-    interval_data = interval_data[start:end]
+def calculate_RSI(data_dict, curr):
+    interval_data = data_dict[curr][start:end]
     rises = 0
     rises_count = 0
     losses = 0
     losses_count = 0
     for i in range(1, len(interval_data)):
-        if interval_data[i - 1] < interval_data[i]:
+        if interval_data[i - 1] <= interval_data[i]:
             rise = interval_data[i] - interval_data[i - 1]
             rises += rise
             rises_count += 1
@@ -70,21 +71,22 @@ def calculate_RSI(data_list):
     return RSI
 
 
-def add_avgRSI(buy_list, sell_list, avg_buy_list, avg_sell_list, RSI_buy_list, RSI_sell_list):
-    buy, sell = get_values(curr)
-    buy_list.append(buy)
-    sell_list.append(sell)
-    buy_avg = calculate_averange(buy_list)
-    sell_avg = calculate_averange(sell_list)
-    avg_buy_list.append(buy_avg)
-    avg_sell_list.append(sell_avg)
-    RSI_buy_list.append(calculate_RSI(buy_list))
-    RSI_sell_list.append(calculate_RSI(sell_list))
-    return avg_buy_list, avg_sell_list, RSI_buy_list, RSI_sell_list
+def add_values(curr, buy, sell, volumen, avg_buy, avg_sell, RSI_buy, RSI_sell):
+    b, s = get_values(curr)
+    buy[curr].append(b)
+    sell[curr].append(s)
+    volumen[curr].append(get_volumen(curr))
+    avg_buy[curr].append(calculate_averange(buy, curr))
+    avg_sell[curr].append(calculate_averange(sell, curr))
+    RSI_buy[curr].append(calculate_RSI(buy, curr))
+    RSI_sell[curr].append(calculate_RSI(sell, curr))
+    return buy, sell, volumen, avg_buy, avg_sell, RSI_buy, RSI_sell
+
 
 
 def draw_axes():
     fig, axs = plt.subplots(3, 1, figsize=(12, 5), constrained_layout=True)
+    locator = MaxNLocator(nbins=4)
     axs[0].set_title(curr)
     axs[0].set_xlabel('Time')
     axs[0].set_ylabel('Value')
@@ -94,25 +96,33 @@ def draw_axes():
     axs[2].set_title('RSI')
     axs[2].set_xlabel('Time')
     axs[2].set_ylabel('Value')
+    axs[0].xaxis.set_major_locator(locator)
+    axs[1].xaxis.set_major_locator(locator)
+    axs[2].xaxis.set_major_locator(locator)
     return fig, axs
 
 
-def draw_plot(time_interval):
+def draw_plot(time_interval, curr):
     fig, axs = draw_axes()
-    buy, sell, volume, values_buy, values_sell, buy_list, sell_list, avg_buy_list, avg_sell_list, volumen_list, rsi_buy_list, rsi_sell_list = [], [], [], [], [], [], [], [], [], [], [], []
-    iterator = 0
+    buy_dict, sell_dict, volumen_dict, avg_buy_dict, avg_sell_dict, RSI_buy_dict, RSI_sell_dict = gen_empty_dicts(
+        list_currencies), gen_empty_dicts(list_currencies), gen_empty_dicts(list_currencies), gen_empty_dicts(
+        list_currencies), gen_empty_dicts(list_currencies), gen_empty_dicts(list_currencies), gen_empty_dicts(
+        list_currencies)
+    current_time = []
     while True:
-        add_values(buy, sell, volume)
-        avg_buy, avg_sell, buy_RSI, sell_RSI = add_avgRSI(buy_list, sell_list, avg_buy_list, avg_sell_list, rsi_buy_list, rsi_sell_list)
-        if len(buy) and len(sell) and len(buy_RSI) and len(sell_RSI) == 2:
-            time = [(iterator - 1) * time_interval, iterator * time_interval]
-            selling_cost = [sell[-2], sell[-1]]
-            purchase_cost = [buy[-2], buy[-1]]
-            volume_value = [volume[-2], volume[-1]]
-            avg_buy_value = [avg_buy[-2], avg_buy[-1]]
-            avg_sell_value = [avg_sell[-2], avg_sell[-1]]
-            RSI_buy_value = [buy_RSI[-2], buy_RSI[-1]]
-            RSI_sell_value = [sell_RSI[-2], sell_RSI[-1]]
+        buy, sell, volumen, avg_buy, avg_sell, RSI_buy, RSI_sell = add_values(curr, buy_dict, sell_dict, volumen_dict,
+                                                                              avg_buy_dict, avg_sell_dict, RSI_buy_dict,
+                                                                              RSI_sell_dict)
+        time_list = get_time(current_time)
+        if len(buy[curr]) and len(sell[curr]) and len(RSI_buy[curr]) and len(RSI_sell[curr]) == 2:
+            time = [time_list[-2].strftime("%H:%M:%S"), time_list[-1].strftime("%H:%M:%S")]
+            selling_cost = [sell[curr][-2], sell[curr][-1]]
+            purchase_cost = [buy[curr][-2], buy[curr][-1]]
+            volume_value = [volumen[curr][-2], volumen[curr][-1]]
+            avg_buy_value = [avg_buy[curr][-2], avg_buy[curr][-1]]
+            avg_sell_value = [avg_sell[curr][-2], avg_sell[curr][-1]]
+            RSI_buy_value = [RSI_buy[curr][-2], RSI_buy[curr][-1]]
+            RSI_sell_value = [RSI_sell[curr][-2], RSI_sell[curr][-1]]
             axs[0].plot(time, selling_cost, label="Selling cost", color='c')
             axs[0].plot(time, purchase_cost, label="Purchase cost", color='limegreen')
             axs[0].plot(time, avg_buy_value, '--', label='Average Buy Cost', color='b')
@@ -124,15 +134,15 @@ def draw_plot(time_interval):
             axs[2].plot(time, RSI_buy_value, label='RSI buy', color='coral' )
             axs[2].plot(time, RSI_sell_value, label='RSI sell', color='darkred')
             axs[2].legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-        elif len(buy) and len(sell) and len(buy_RSI) and len(sell_RSI) > 2:
-            time = [(iterator - 1) * time_interval, iterator * time_interval]
-            selling_cost = [sell[-2], sell[-1]]
-            purchase_cost = [buy[-2], buy[-1]]
-            volume_value = [volume[-2], volume[-1]]
-            avg_buy_value = [avg_buy[-2], avg_buy[-1]]
-            avg_sell_value = [avg_sell[-2], avg_sell[-1]]
-            RSI_buy_value = [buy_RSI[-2], buy_RSI[-1]]
-            RSI_sell_value = [sell_RSI[-2], sell_RSI[-1]]
+        elif len(buy[curr]) and len(sell[curr]) and len(RSI_buy[curr]) and len(RSI_sell[curr]) > 2:
+            time = [time_list[-2].strftime("%H:%M:%S"), time_list[-1].strftime("%H:%M:%S")]
+            selling_cost = [sell[curr][-2], sell[curr][-1]]
+            purchase_cost = [buy[curr][-2], buy[curr][-1]]
+            volume_value = [volumen[curr][-2], volumen[curr][-1]]
+            avg_buy_value = [avg_buy[curr][-2], avg_buy[curr][-1]]
+            avg_sell_value = [avg_sell[curr][-2], avg_sell[curr][-1]]
+            RSI_buy_value = [RSI_buy[curr][-2], RSI_buy[curr][-1]]
+            RSI_sell_value = [RSI_sell[curr][-2], RSI_sell[curr][-1]]
             axs[0].plot(time, selling_cost, color="c")
             axs[0].plot(time, purchase_cost, color='limegreen')
             axs[0].plot(time, avg_buy_value,'--', label='Average Buy Cost', color='b')
@@ -141,8 +151,6 @@ def draw_plot(time_interval):
             axs[1].set_xticks([])
             axs[2].plot(time, RSI_buy_value, label='RSI buy', color='coral' )
             axs[2].plot(time, RSI_sell_value, label='RSI sell', color='darkred')
-
-        iterator += 1
         plt.pause(time_interval)
 
 
@@ -151,11 +159,11 @@ if __name__ == '__main__':
         list_currencies = ['BTC-PLN', 'LTC-PLN', 'TRX-PLN']
         time_interval = 2
         i = int(input('Podaj dla jakiej waluty wykres chcesz wyswietlic 0-BTC, 1-LTC, 2-TRX: '))
-        qty = int(input('Ilość próbek do wyliczenia średniej: '))
-        start = int(input('Początek przedziału y do wyliczenia RSI: '))
-        end = int(input('Koniec przedziału y do wyliczenia RSI (maksymalnie 20): '))
+        qty = 10
+        start = 0
+        end = 10
         curr = list_currencies[i]
-        draw_plot(time_interval)
+        draw_plot(time_interval, curr)
     except KeyboardInterrupt:
         print('User hit the interrupt key')
         sys.exit()
